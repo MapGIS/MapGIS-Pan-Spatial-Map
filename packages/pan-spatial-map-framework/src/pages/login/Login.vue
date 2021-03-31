e5087c42c9e678bf55fea74f4ddcb27b83269ec1
xionglaoda <xiongbo@mapgis.com>
2021-03-31 13:00:33 +0800

    update login page

--- packages/pan-spatial-map-framework/src/pages/login/Login.vue
+++ packages/pan-spatial-map-framework/src/pages/login/Login.vue
@@ -1,93 +1,90 @@
 <template>
-  <div class="common-layout">
-    <div class="content">
-      <div class="top">
-        <div class="header">
-          <img alt="logo" class="logo" src="@/assets/img/logo.png" />
-          <span class="title">{{ systemName }}</span>
-        </div>
-        <div class="desc"></div>
+  <common-layout class="login-wrapper">
+    <div class="top">
+      <div class="header">
+        <img alt="logo" class="logo" src="@/assets/img/logo.png" />
+        <span class="title">{{ systemName }}</span>
       </div>
-      <div class="login">
-        <a-form @submit="onSubmit" :form="form">
-          <a-alert
-            type="error"
-            :closable="true"
-            v-if="error"
-            :message="error"
-            @close="onClose"
-            showIcon
-            style="margin-bottom: 24px"
-          />
-          <a-form-item>
-            <a-input
-              autocomplete="autocomplete"
-              size="large"
-              v-decorator="[
-                'name',
-                {
-                  rules: [
-                    {
-                      required: true,
-                      message: '请输入账户名',
-                      whitespace: true
-                    }
-                  ]
-                }
-              ]"
-            >
-              <a-icon slot="prefix" type="user" />
-            </a-input>
-          </a-form-item>
-          <a-form-item>
-            <a-input
-              size="large"
-              autocomplete="autocomplete"
-              type="password"
-              v-decorator="[
-                'password',
-                {
-                  rules: [
-                    {
-                      required: true,
-                      message: '请输入密码',
-                      whitespace: true
-                    }
-                  ]
-                }
-              ]"
-            >
-              <a-icon slot="prefix" type="lock" />
-            </a-input>
-          </a-form-item>
-          <a-form-item>
-            <a-button
-              :loading="logging"
-              style="width: 100%; margin-top: 24px"
-              size="large"
-              htmlType="submit"
-              type="primary"
-              >登录
-            </a-button>
-          </a-form-item>
-        </a-form>
-      </div>
+      <div class="desc"></div>
     </div>
-    <div class="footer">
-      <div class="copyright">
-        Copyright<a-icon type="copyright" />{{ copyright }}
-      </div>
+    <div class="login">
+      <a-form @submit="onSubmit" :form="form">
+        <a-alert
+          type="error"
+          :closable="true"
+          v-if="error"
+          :message="error"
+          @close="onClose"
+          showIcon
+          style="margin-bottom: 24px"
+        />
+        <a-form-item>
+          <a-input
+            autocomplete="autocomplete"
+            size="large"
+            placeholder="用户名"
+            v-decorator="[
+              'name',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入用户名',
+                    whitespace: true
+                  }
+                ]
+              }
+            ]"
+          >
+            <a-icon slot="prefix" type="user" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input
+            size="large"
+            placeholder="密码"
+            autocomplete="autocomplete"
+            type="password"
+            v-decorator="[
+              'password',
+              {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入密码',
+                    whitespace: true
+                  }
+                ]
+              }
+            ]"
+          >
+            <a-icon slot="prefix" type="lock" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button
+            :loading="logging"
+            style="width: 100%; margin-top: 24px"
+            size="large"
+            htmlType="submit"
+            type="primary"
+            >登录
+          </a-button>
+        </a-form-item>
+      </a-form>
     </div>
-  </div>
+  </common-layout>
 </template>
 
 <script>
+import CommonLayout from '@/layouts/CommonLayout'
 import { login } from '@/services/user'
 import { setAuthorization } from '@/utils/request'
 import { mapMutations } from 'vuex'
 
 export default {
   name: 'Login',
+  components: { CommonLayout },
   data() {
     return {
       logging: false,
@@ -99,9 +96,6 @@ export default {
   computed: {
     systemName() {
       return this.$store.state.setting.systemName
-    },
-    copyright() {
-      return this.$store.state.setting.copyright
     }
   },
   watch: {
@@ -149,96 +143,86 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
-.common-layout {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  overflow: auto;
-  background-color: @layout-body-background;
-  background-image: url('../../assets/img/login_bg.png');
-  background-repeat: no-repeat;
-  background-position-x: 50%;
-  background-position-y: center;
-  background-size: 100%;
-  .content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-left: 50vw;
-    @media screen and (max-width: 480px) {
-      margin-left: 0;
+<style lang="less">
+.login-wrapper {
+  &.common-layout {
+    background-image: url('../../assets/img/login-bg.png');
+    background-repeat: no-repeat;
+    background-position-x: center;
+    background-size: cover;
+    .content {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
     }
-    padding: 32px 0;
-    flex: 1;
-    @media (min-width: 768px) {
-      padding: 112px 0 24px;
-    }
-    .top {
-      text-align: center;
-      .header {
-        height: 44px;
-        line-height: 44px;
+    .footer {
+      .copyright {
+        color: rgba(255, 255, 255, 0.45);
+      }
+      .links {
         a {
-          text-decoration: none;
-        }
-        .logo {
-          height: 44px;
-          vertical-align: top;
-          margin-right: 16px;
-        }
-        .title {
-          font-size: 33px;
-          color: @title-color;
-          font-family: 'Myriad Pro', 'Helvetica Neue', Arial, Helvetica,
-            sans-serif;
-          font-weight: 600;
-          position: relative;
-          top: 2px;
-        }
-      }
-      .desc {
-        font-size: 14px;
-        color: @text-color-second;
-        margin-top: 12px;
-        margin-bottom: 40px;
-      }
-    }
-    .login {
-      width: 368px;
-      margin: 0 auto;
-      @media screen and (max-width: 576px) {
-        width: 95%;
-      }
-      @media screen and (max-width: 320px) {
-        .captcha-button {
-          font-size: 14px;
-        }
-      }
-      .icon {
-        font-size: 24px;
-        color: @text-color-second;
-        margin-left: 16px;
-        vertical-align: middle;
-        cursor: pointer;
-        transition: color 0.3s;
-
-        &:hover {
-          color: @primary-color;
+          color: rgba(255, 255, 255, 0.45);
         }
       }
     }
   }
-  .footer {
-    padding: 48px 16px 24px;
-    /*margin: 48px 0 24px;*/
+}
+</style>
+
+<style lang="less" scoped>
+.common-layout {
+  .top {
     text-align: center;
-    .copyright {
-      color: @text-color-second;
+    .header {
+      height: 44px;
+      line-height: 44px;
+      a {
+        text-decoration: none;
+      }
+      .logo {
+        height: 44px;
+        vertical-align: top;
+        margin-right: 16px;
+      }
+      .title {
+        font-size: 33px;
+        color: #dbdfec;
+        font-family: 'Myriad Pro', 'Helvetica Neue', Arial, Helvetica,
+          sans-serif;
+        font-weight: 600;
+        position: relative;
+        top: 2px;
+      }
+    }
+    .desc {
       font-size: 14px;
-      i {
-        margin: 0 4px;
+      color: @text-color-second;
+      margin-top: 12px;
+      margin-bottom: 40px;
+    }
+  }
+  .login {
+    width: 368px;
+    margin: 0 auto;
+    @media screen and (max-width: 576px) {
+      width: 95%;
+    }
+    @media screen and (max-width: 320px) {
+      .captcha-button {
+        font-size: 14px;
+      }
+    }
+    .icon {
+      font-size: 24px;
+      color: @text-color-second;
+      margin-left: 16px;
+      vertical-align: middle;
+      cursor: pointer;
+      transition: color 0.3s;
+
+      &:hover {
+        color: @primary-color;
       }
     }
   }
