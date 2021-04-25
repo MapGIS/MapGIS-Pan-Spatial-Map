ad6d5de73fe23a4e97198527c5d4d2b5673f3450
helong <hl13886803841@sina.com>
2021-04-25 19:03:43 +0800

    完成了标注微件的大部分样式

---
+++ packages/pan-spatial-map-plugin-editor/src/widgets/marker-manager/components/MarkerInput/MarkerInput.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="marker-input">
+    <a-form-model
+      :model="formInput"
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 16 }"
+    >
+      <a-form-model-item label="坐标单位:">
+        <a-select v-model="formInput.unit">
+          <a-select-option v-for="item in unitTypes" :key="item">{{
+            item
+          }}</a-select-option>
+        </a-select>
+      </a-form-model-item>
+      <a-form-model-item label="X坐标:">
+        <a-input
+          v-show="formInput.unit === '十进制'"
+          v-model.number="formInput.coordX"
+          type="number"
+        >
+        </a-input>
+        <div v-show="formInput.unit === '度分秒'" class="coord-input">
+          <a-input v-model="formInput.degreeX" type="number"> </a-input>
+          <span>度</span>
+          <a-input v-model="formInput.minuteX" type="number"> </a-input>
+          <span>分</span>
+          <a-input v-model="formInput.secondX" type="number"> </a-input>
+          <span>秒</span>
+        </div>
+      </a-form-model-item>
+      <a-form-model-item label="Y坐标:">
+        <a-input
+          v-show="formInput.unit === '十进制'"
+          v-model.number="formInput.coordY"
+          type="number"
+        >
+        </a-input>
+        <div v-show="formInput.unit === '度分秒'" class="coord-input">
+          <a-input v-model="formInput.degreeY" type="number"> </a-input>
+          <span>度</span>
+          <a-input v-model="formInput.minuteY" type="number"> </a-input>
+          <span>分</span>
+          <a-input v-model="formInput.secondY" type="number"> </a-input>
+          <span>秒</span>
+        </div>
+      </a-form-model-item>
+      <a-checkbox class="crs-checkbox" @change="onChange">
+        设置输入坐标空间参考系
+      </a-checkbox>
+      <a-form-model-item v-show="showCrsSelect" label="参考系:">
+        <a-select v-model="formInput.crsName">
+          <a-select-option v-for="item in crsNames" :key="item">{{
+            item
+          }}</a-select-option>
+        </a-select>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
+</template>
+
+<script lang="ts">
+import { Mixins, Component, Emit } from 'vue-property-decorator'
+import { utilInstance } from '@mapgis/pan-spatial-map-store'
+import { ThemeStyleMixin } from '@mapgis/web-app-framework'
+import { UUID } from '@mapgis/webclient-store/src/utils'
+import markerBlue from '../../../../assets/images/markerBlue.png'
+import MarkerAddMixin from '../../mixins/marker-add'
+
+@Component({ name: 'MpMarkerInput' })
+export default class MpMarkerInput extends Mixins() {
+  // 表单数据
+  private formInput = {
+    unit: '十进制',
+    coordX: 0,
+    coordY: 0,
+    degreeX: 0,
+    minuteX: 0,
+    secondX: 0,
+    degreeY: 0,
+    minuteY: 0,
+    secondY: 0,
+    crsName: 'WGS1984_度'
+  }
+
+  private unitTypes = ['十进制', '度分秒']
+
+  private crsNames = ['WGS1984_度', 'Web墨卡托_WGS1984']
+
+  // 参考系选择器的显隐
+  private showCrsSelect = true
+
+  // 多选框变化时回调函数
+  onChange(e) {
+    this.showCrsSelect = e.target.checked
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.ant-form-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0;
+}
+::v-deep .ant-form-item-label {
+  width: 104px;
+}
+::v-deep .ant-form-item-control-wrapper {
+  width: 208px;
+}
+
+.crs-checkbox {
+  margin: 8px 0 8px 20px;
+}
+.coord-input {
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+}
+</style>
--- packages/pan-spatial-map-plugin-editor/src/widgets/marker-manager/marker-manager.vue
+++ packages/pan-spatial-map-plugin-editor/src/widgets/marker-manager/marker-manager.vue
@@ -1,13 +1,205 @@
 <template>
-  <div class="mp-widget-marker-manager">标注管理</div>
+  <div class="mp-widget-marker-manager">
+    <div class="btn-tooltip">
+      <div class="marker-input">
+        <div class="marker-mouse">
+          <div>
+            <a-tooltip
+              class="mouse-item"
+              v-for="item in markMouseModes"
+              :key="item.title"
+              placement="bottom"
+            >
+              <template slot="title">{{ item.title }}</template>
+              <a-button type="circle" :icon="item.icon"></a-button>
+            </a-tooltip>
+          </div>
+          <span>鼠标交互</span>
+        </div>
+        <a-divider type="vertical" />
+        <div class="marker-keyboard">
+          <div class="keyboard-btns">
+            <a-button
+              class="keyboard-item"
+              v-for="item in markKeyboardModes"
+              :key="item.title"
+              type="circle"
+              :icon="item.icon"
+              @click="item.click"
+            ></a-button>
+          </div>
+          <span>键盘导入</span>
+        </div>
+        <a-divider type="vertical" />
+      </div>
+      <div class="marker-manager">
+        <div
+          class="manager-item"
+          v-for="item in markManagerModes"
+          :key="item.title"
+        >
+          <a-button type="circle" :icon="item.icon"></a-button>
+          <span>{{ item.title }}</span>
+        </div>
+      </div>
+    </div>
+    <a-table
+      :row-selection="{
+        selectedRowKeys: selectedRowKeys,
+        onChange: onSelectChange
+      }"
+      :columns="columns"
+      :data-source="tableData"
+      :pagination="pagination"
+    >
+    </a-table>
+    <a-modal v-model="modalInput" title="输入坐标" :width="360">
+      <marker-input />
+    </a-modal>
+  </div>
 </template>
 
 <script lang="ts">
-import { Mixins, Component } from 'vue-property-decorator'
+import { Mixins, Component, Inject, Watch } from 'vue-property-decorator'
 import { WidgetMixin } from '@mapgis/web-app-framework'
+import MarkerInput from './components/MarkerInput/MarkerInput.vue'
 
-@Component({ name: 'MpMarkerManager' })
-export default class MpMarkerManager extends Mixins(WidgetMixin) {}
+@Component({ name: 'MpMarkerManager', components: { MarkerInput } })
+export default class MpMarkerManager extends Mixins(WidgetMixin) {
+  private markMouseModes = [
+    { mode: 'point', title: '点', icon: 'environment' },
+    {
+      mode: 'line',
+      title: '线',
+      icon: 'pull-request'
+    },
+    {
+      mode: 'polygon',
+      title: '区',
+      icon: 'area-chart'
+    }
+  ]
+
+  private markKeyboardModes = [
+    {
+      title: '键盘',
+      icon: 'laptop',
+      click: this.addMarkerByInputCoord
+    },
+    {
+      title: '导入',
+      icon: 'import',
+      click: this.addMarkerByImportFile
+    }
+  ]
+
+  private markManagerModes = [
+    {
+      title: '保存',
+      icon: 'save'
+    },
+    {
+      title: '导出',
+      icon: 'export'
+    },
+    {
+      title: '删除',
+      icon: 'delete'
+    }
+  ]
+
+  // 表格列的配置描述
+  private columns = [
+    {
+      title: '名称',
+      dataIndex: 'name',
+      align: 'center',
+      sorter: true
+    },
+    {
+      title: '描述',
+      dataIndex: 'description',
+      align: 'center',
+      sorter: true
+    }
+  ]
+
+  // 表格数据
+  private tableData: any[] = [
+    {
+      key: 1,
+      name: 'aaa',
+      description: 'a1aa'
+    },
+    {
+      key: 2,
+      name: 'bbb',
+      description: 'b2bb'
+    }
+  ]
+
+  // 指定选中项的 key 数组
+  private selectedRowKeys = []
+
+  // 分页器配置
+  private pagination = {
+    showSizeChanger: true,
+    size: 'small'
+  }
+
+  // 键盘对话框的显隐
+  private modalInput = false
+
+  // 导入对话框的显隐
+  private modalImport = false
+
+  // 导出对话框的显隐
+  private modalExport = false
+
+  // 选中项发生变化时的回调
+  onSelectChange(selectedRowKeys) {
+    this.selectedRowKeys = selectedRowKeys
+  }
+
+  // 键盘按钮回调事件
+  addMarkerByInputCoord() {
+    this.modalInput = true
+  }
+
+  // 导入按钮回调事件
+  addMarkerByImportFile() {}
+}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.btn-tooltip {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.marker-input,
+.marker-manager,
+.keyboard-btns {
+  display: flex;
+  align-items: center;
+}
+
+.marker-mouse,
+.marker-keyboard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.manager-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.mouse-item,
+.keyboard-item,
+.manager-item {
+  margin-right: 4px;
+}
+</style>
