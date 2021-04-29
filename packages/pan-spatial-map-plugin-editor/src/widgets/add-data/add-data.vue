b342376f27b8d853ecf863f75738ca795ea1389d
helong <hl13886803841@sina.com>
2021-04-29 19:02:26 +0800

    开始开发添加服务微件

--- packages/pan-spatial-map-plugin-editor/src/widgets/add-data/add-data.vue
+++ packages/pan-spatial-map-plugin-editor/src/widgets/add-data/add-data.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="mp-widget-add-data">添加数据</div>
+  <div class="mp-widget-add-data">
+    <a-tabs default-active-key="1">
+      <a-tab-pane key="1" tab="数据列表">
+        <add-services-data :service-types="allTypes" />
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="URL">
+        <div>URL</div>
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="文件">
+        <div>文件</div>
+      </a-tab-pane>
+    </a-tabs>
+  </div>
 </template>
 
 <script lang="ts">
-import { Mixins, Component } from 'vue-property-decorator'
+import { Mixins, Component, Watch } from 'vue-property-decorator'
 import { WidgetMixin } from '@mapgis/web-app-framework'
+import {
+  AddServicesMixin,
+  ServiceType,
+  Service,
+  queryIgsServicesInfoInstance
+} from '@mapgis/pan-spatial-map-store'
+import AddServicesData from './components/AddServicesData.vue'
 
-@Component({ name: 'MpAddData' })
-export default class MpAddData extends Mixins(WidgetMixin) {}
+@Component({
+  name: 'MpAddData',
+  components: {
+    AddServicesData
+  }
+})
+export default class MpAddData extends Mixins(WidgetMixin) {
+  private allTypes: ServiceType[] = []
+}
 </script>
 
 <style lang="less" scoped></style>
---
+++ packages/pan-spatial-map-plugin-editor/src/widgets/add-data/components/AddServicesData.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="add-services-data">
+    <div class="row">
+      <!-- <a-select v-model="serviceCategory">
+        <a-select-option v-for="item in serviceCategories" :key="item.name">{{
+          item.name
+        }}</a-select-option>
+      </a-select>
+      <a-select v-model="serviceType">
+        <a-select-option v-for="item in serviceTypes" :key="item">{{
+          item
+        }}</a-select-option>
+      </a-select> -->
+    </div>
+    <div class="row"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Mixins, Component, Prop, Watch } from 'vue-property-decorator'
+import { AppMixin } from '@mapgis/web-app-framework'
+import {
+  AddServicesMixin,
+  ServiceType,
+  Service
+} from '@mapgis/pan-spatial-map-store'
+import axios from 'axios'
+
+@Component({})
+export default class AddServicesData extends Mixins(
+  AddServicesMixin,
+  AppMixin
+) {
+  @Prop(Array) readonly serviceTypes!: ServiceType[]
+
+  // 类别选中项
+  private serviceCategory: any = { name: '', desc: '' }
+
+  // 子类别选中项
+  private serviceType = null
+
+  created() {
+    console.log(this.serviceCategories)
+
+    this.serviceCategory = this.serviceCategories[0]
+    this.serviceType = this.serviceTypes[0]
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
