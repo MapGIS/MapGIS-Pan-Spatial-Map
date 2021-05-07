b909c3a21332f80761347bb46d51fd2a774f30c9
helong <hl13886803841@sina.com>
2021-05-07 09:19:00 +0800

    暂存添加服务代码

--- packages/pan-spatial-map-plugin-editor/src/widgets/add-data/components/MapGISSelects/IgsServer.vue
+++ packages/pan-spatial-map-plugin-editor/src/widgets/add-data/components/MapGISSelects/IgsServer.vue
@@ -1,15 +1,69 @@
 <template>
-  <div></div>
+  <div class="igs-server">
+    <div class="input-item">
+      <label class="">IP地址：</label>
+      <a-input v-model="ip"></a-input>
+    </div>
+    <div class="input-item">
+      <label class="">端口：</label>
+      <a-input v-model="port"></a-input>
+    </div>
+    <div class="input-item">
+      <label>GDBP地址：</label>
+      <mapgis-layer :ip="ip" :port="port" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
+import MapgisLayer from './MapgisLayer.vue'
+import MapgisServer from './MapgisServer.vue'
 
 @Component({
   name: 'IgsServer',
-  components: {}
+  components: { MapgisLayer, MapgisServer }
 })
-export default class IgsServer extends Vue {}
+export default class IgsServer extends Vue {
+  @Prop({ type: String, default: 'doc' }) readonly dataType!: string
+
+  // 服务器ip
+  private ip = 'develop.smaryun.com'
+
+  // 服务器端口
+  private port = '6163'
+
+  get showLayer() {
+    return this.dataType === 'layer'
+  }
+
+  get showMap() {
+    return this.dataType === 'doc' || this.dataType === 'tile'
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.igs-server {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.input-item {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+
+  label {
+    width: 79px;
+  }
+
+  .ant-input {
+    margin-left: 4px;
+    flex-grow: 1;
+  }
+}
+</style>
