357b6fd819893ae14a726e9f145b3509d87e59c7
zhuxiuli <zhuxiuli.julie@mapgis.com>
2021-05-08 19:33:50 +0800

    feat: 对接放大缩小等基本功能

--- packages/pan-spatial-map-plugin-visualization/src/widgets/split-screen/components/MapView/index.vue
+++ packages/pan-spatial-map-plugin-visualization/src/widgets/split-screen/components/MapView/index.vue
@@ -5,11 +5,23 @@
         <a-icon :type="item.icon" @click.stop="onSettingIconClick(item)" />
       </a-tooltip>
     </div>
-    <mp-mapbox-view :document="document" :mapStyle="mapStyle" />
+    <mp-mapbox-view
+      :document="document"
+      :mapStyle="mapStyle"
+      @map-load="onMapLoad"
+      v-if="document"
+    />
+    <mapgis-draw
+      v-if="isMapLoaded"
+      ref="mapboxBaseDrawer"
+      :controls="controls"
+      @added="onAdded"
+      @drawCreate="onGraphicCreated"
+    />
   </div>
 </template>
 <script lang="ts">
-import { Mixins, Component, Prop } from 'vue-property-decorator'
+import { Mixins, Component, Prop, Watch } from 'vue-property-decorator'
 import {
   Document,
   WidgetMixin,
@@ -17,24 +29,68 @@ import {
   Layer
 } from '@mapgis/web-app-framework'
 import mapViewOperationMixin from '../../mixins/map-view-operation-mixin'
+import { Rect } from '../../mixins/map-view-state'
 
 type Noop = (l: Layer) => void
+
+type OperationType =
+  | 'UNKNOW'
+  | 'QUERY'
+  | 'ZOOMIN'
+  | 'ZOOMOUT'
+  | 'RESORT'
+  | 'PAN'
+  | 'CLEAR'
 
 interface ITool {
   label: string
   icon: string
-  method: string
+  operationType: OperationType
 }
 
 @Component({
   components: {
     MpMapboxView
+  },
+  provide() {
+    const self = this
+    return {
+      get mapbox() {
+        return self.mapbox
+      },
+      get map() {
+        return self.map
+      }
+    }
   }
 })
 export default class MapView extends Mixins<{
   [k: string]: any
 }>(mapViewOperationMixin) {
+  @Prop() mapViewID!: string
+
   @Prop({ default: () => ({}) }) layer!: Layer
+
+  map: any = {}
+
+  mapbox: any = {}
+
+  isMapLoaded = false
+
+  operationType: OperationType = 'UNKNOW'
+
+  controls = {
+    point: false,
+    line_string: false,
+    polygon: false,
+    trash: false,
+    combine_features: false,
+    uncombine_features: false
+  }
+
+  mapboxBaseDrawer: any = null
+
+  mapboxBaseDrawComponent: any = null
 
   // 图层样式
   mapStyle: any = {
@@ -74,33 +130,33 @@ export default class MapView extends Mixins<{
     {
       label: '查询',
       icon: 'search',
-      method: 'query'
+      operationType: 'query'
     },
     {
       label: '放大',
       icon: 'zoom-in',
-      method: 'zoomIn'
+      operationType: 'zoomIn'
     },
     {
       label: '缩小',
       icon: 'zoom-out',
-      method: 'zoomOut'
+      operationType: 'zoomOut'
     },
 
     {
       label: '复位',
       icon: 'redo',
-      method: 'restore'
+      operationType: 'resort'
     },
     {
       label: '移动',
       icon: 'drag',
-      method: 'move'
+      operationType: 'pan'
     },
     {
       label: '清除',
       icon: 'delete',
-      method: 'clear'
+      operationType: 'clear'
     }
   ]
 
@@ -120,40 +176,177 @@ export default class MapView extends Mixins<{
    * @param item<object>
    */
   onSettingIconClick(item: ITool) {
-    const fn: Noop = this[item.method]
+    const fn: Noop = this[`${item.operationType}Click`]
     if (typeof fn === 'function') {
-      fn(this.layer)
+      this.operationType = item.operationType.toUpperCase()
+      fn()
+    }
+  }
+
+  /**
+   * 地图初始化
+   * @param payload<object>
+   */
+  onMapLoad({ map, mapbox }) {
+    this.map = map
+    this.mapbox = mapbox
+    this.map.on('mousemove', () => (this.activeMapViewID = this.mapViewID))
+    this.map.on('move', () => {
+      if (this.mapViewID && this.activeMapViewID === this.mapViewID) {
+        const { _sw, _ne } = this.map.getBounds()
+        this.activeMapViewDisplayRect = {
+          xMin: _sw.lng,
+          yMin: _sw.lat,
+          xMax: _ne.lng,
+          yMax: _ne.lat
+        }
+      }
+    })
+    this.isMapLoaded = true
+    this.onRestoreBtnClicked()
+  }
+
+  /**
+   * 添加绘制
+   */
+  onAdded(e) {
+    this.mapboxBaseDrawer = e.drawer
+  }
+
+  /**
+   * 创建绘制
+   */
+  onGraphicCreated(e) {
+    const { id, geometry } = e.features[0]
+    this.$delete(this.mapboxBaseDrawer, id)
+    let xmin: number
+    let xmax: number
+    let ymin: number
+    let ymax: number
+    geometry.coordinates[0].forEach(([lng, lat]) => {
+      if (!xmin || lng < xmin) {
+        xmin = lng
+      }
+      if (!xmax || lng > xmax) {
+        xmax = lng
+      }
+      if (!ymin || lat < ymin) {
+        ymin = lat
+      }
+      if (!ymax || lat > ymax) {
+        ymax = lat
+      }
+    })
+    const rect: Rect = new Rect(xmin, ymin, xmax, ymax)
+    switch (this.operationType) {
+      case 'QUERY':
+        this.query(rect)
+        break
+      case 'ZOOMIN':
+        this.zoomIn(rect)
+        break
+      case 'ZOOMOUT':
+        this.zoomOut(rect)
+        break
+      case 'RESORT':
+        break
+      case 'PAN':
+        break
+      case 'CLEAR':
+        break
+      default:
+        break
+    }
+  }
+
+  /**
+   * 启用绘制
+   */
+  enableDrawer() {
+    this.mapboxBaseDrawComponent = this.$refs.mapboxBaseDrawer
+    if (this.mapboxBaseDrawComponent) {
+      this.mapboxBaseDrawComponent.enableDrawer()
+    }
+
+    if (this.mapboxBaseDrawer) {
+      this.mapboxBaseDrawer.changeMode('draw_rectangle')
+    }
+  }
+
+  /**
+   * 启用拖拽,该函数在MapboxBaseDraw开启时不起作用。
+   * @param isEnable<boolean>
+   */
+  enableDragPanMap(isEnable: boolean) {
+    if (this.isMapLoaded) {
+      if (isEnable) {
+        this.map.dragPan.enable()
+      } else {
+        this.map.dragPan.disable()
+      }
+    }
+  }
+
+  /**
+   * 跳转值矩形区域
+   * @param displayRect<object>
+   */
+  jumpToRect(displayRect: Rect) {
+    if (this.isMapLoaded) {
+      this.map.fitBounds([
+        [displayRect.xMax, displayRect.yMin],
+        [displayRect.xMin, displayRect.yMax]
+      ])
     }
   }
 
   /**
    * 查询
    */
-  query() {}
+  queryClick() {
+    this.enableDrawer()
+  }
 
   /**
    * 放大
    */
-  zoomOut() {
-    // this.operationType = OperationType.ZOOMIN
-    this.enableDragePanMap(false)
+  zoomInClick() {
+    this.enableDragPanMap(false)
     this.enableDrawer()
   }
 
   /**
    * 缩小
    */
-  zoomIn() {}
+  zoomOutClick() {
+    this.zoomInClick()
+  }
 
   /**
-   * 重置
+   * 复位
    */
-  restore() {}
+  resortClick() {
+    this.jumpToRect(this.initDisplayRect)
+  }
+
+  /**
+   * 拖拽
+   */
+  panClick() {
+    this.enableDragPanMap(true)
+  }
 
   /**
    * 清除
    */
-  clear() {}
+  clearClick() {}
+
+  @Watch('activeMapViewDisplayRect', { deep: true })
+  onActiveMapViewDisplayRectChanged(nV) {
+    if (this.mapViewID !== this.activeMapViewID) {
+      this.jumpToRect(nV)
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
--- packages/pan-spatial-map-plugin-visualization/src/widgets/split-screen/mixins/map-view-operation-mixin.ts
+++ packages/pan-spatial-map-plugin-visualization/src/widgets/split-screen/mixins/map-view-operation-mixin.ts
@@ -1,6 +1,6 @@
 import { Vue, Component, Prop } from 'vue-property-decorator'
 
-import { Rect } from './map-view-state'
+import mapViewStateInstance, { MapViewState, Rect } from './map-view-state'
 
 /**
  * 二维地图操作混入,提供操作地图的常用接口
@@ -10,17 +10,71 @@ import { Rect } from './map-view-state'
  */
 @Component
 export default class MapViewOperationMixin extends Vue {
-  private map: any = {}
+  map: any = {}
 
-  private mapbox: any = {}
+  mapbox: any = {}
+
+  activeMapViewState: MapViewState = mapViewStateInstance
+
+  showQueryResult = false
+
+  queryRect: Rect | object = {}
+
+  // 获取当前激活的地图视图的范围
+  get activeMapViewDisplayRect(): Rect {
+    return this.activeMapViewState.displayRect
+  }
+
+  // 设置当前激活的地图视图的范围
+  set activeMapViewDisplayRect(displayRect: Rect) {
+    this.activeMapViewState.displayRect = displayRect
+  }
+
+  // 获取地图视图的复位范围
+  get initDisplayRect(): Rect {
+    return this.activeMapViewState.initDisplayRect
+  }
+
+  // 设置地图视图的复位范围
+  set initDisplayRect(initDisplayRect: Rect) {
+    this.activeMapViewState.initDisplayRect = initDisplayRect
+  }
+
+  // 设置当前激活的地图视图的ID
+  set activeMapViewID(ID: string) {
+    this.activeMapViewState.mapViewID = ID
+  }
+
+  // 获取当前激活的地图视图的ID
+  get activeMapViewID(): string {
+    return this.activeMapViewState.mapViewID
+  }
 
   /**
-   * 放大地图到指定区域
-   * 可用于拉框放大
-   * @param {Rect} rect 指定区域
+   * 判断矩形范围是否可用
+   * @private
+   * @param {Rect} rect
+   * @return {*} false:不可用,true:可用
    * @memberof MpMapViewOperationMixin
    */
-  public zoomIn(rect: Rect) {
+  isValidRect(rect: Rect) {
+    return rect && rect.xMin < rect.xMax && rect.yMin < rect.yMax
+  }
+
+  /**
+   * 查询地图
+   * @param {Rect} rect 指定区域
+   */
+  query(rect: Rect) {
+    this.showQueryResult = true
+    this.queryRect = rect
+  }
+
+  /**
+   * 放大地图到指定区域的中心
+   * @param {Rect} rect 指定区域
+   */
+  zoomIn(rect: Rect) {
     if (this.isValidRect(rect)) {
       this.map.fitBounds([
         [rect.xMax, rect.yMin],
@@ -34,25 +88,13 @@ export default class MapViewOperationMixin extends Vue {
   /**
    * 缩小地图到指定区域的中心
    * @param {Rect} rect 指定区域
-   * @memberof MpMapViewOperationMixin
    */
-  public zoomOut(rect: Rect) {
+  zoomOut(rect: Rect) {
     if (this.isValidRect(rect)) {
       this.map.flyTo({
         zoom: this.map.getZoom() - 1,
         center: [(rect.xMin + rect.xMin) / 2, (rect.yMin + rect.yMax) / 2]
       })
     }
-  }
-
-  /**
-   * 判断矩形范围是否可用
-   * @private
-   * @param {Rect} rect
-   * @return {*} false:不可用,true:可用
-   * @memberof MpMapViewOperationMixin
-   */
-  private isValidRect(rect: Rect) {
-    return rect && rect.xMin < rect.xMax && rect.yMin < rect.yMax
   }
 }
