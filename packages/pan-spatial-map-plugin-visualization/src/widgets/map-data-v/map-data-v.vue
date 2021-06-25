bccbc13879f8ba4a3ad5187b116a1c1a1ef4f599
helong <hl13886803841@sina.com>
2021-06-25 18:17:45 +0800

    refactor: 设置iframe为DataV的示例，且默认全屏显示

--- packages/pan-spatial-map-plugin-visualization/src/widgets/map-data-v/map-data-v.vue
+++ packages/pan-spatial-map-plugin-visualization/src/widgets/map-data-v/map-data-v.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="mp-widget-map-data-v">
     <iframe
-      src="https://www.baidu.com/"
+      id="iframe_page"
+      src="http://datav.jiaminghi.com/demo/construction-data/index.html"
       frameborder="0"
       width="100%"
       height="100%"
+      :allowfullscreen="true"
     ></iframe>
   </div>
 </template>
@@ -14,7 +16,12 @@ import { Mixins, Component } from 'vue-property-decorator'
 import { WidgetMixin } from '@mapgis/web-app-framework'
 
 @Component({ name: 'MpMapDataV' })
-export default class MpMapDataV extends Mixins(WidgetMixin) {}
+export default class MpMapDataV extends Mixins(WidgetMixin) {
+  onOpen() {
+    const page = document.getElementById('iframe_page')
+    page.requestFullscreen()
+  }
+}
 </script>
 
 <style lang="less" scoped>
