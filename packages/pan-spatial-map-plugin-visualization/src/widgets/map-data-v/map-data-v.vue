4848c7609d38e5090bb4ed2d91553a64ce0da156
helong <hl13886803841@sina.com>
2021-07-07 16:59:05 +0800

    perf: 重写了全屏事件

--- packages/pan-spatial-map-plugin-visualization/src/widgets/map-data-v/map-data-v.vue
+++ packages/pan-spatial-map-plugin-visualization/src/widgets/map-data-v/map-data-v.vue
@@ -2,7 +2,7 @@
   <div class="mp-widget-map-data-v">
     <iframe
       id="iframe_page"
-      src="http://datav.jiaminghi.com/demo/construction-data/index.html"
+      :src="url"
       frameborder="0"
       width="100%"
       height="100%"
@@ -13,13 +13,25 @@
 
 <script lang="ts">
 import { Mixins, Component } from 'vue-property-decorator'
-import { WidgetMixin } from '@mapgis/web-app-framework'
+import { WidgetMixin, DomUtil } from '@mapgis/web-app-framework'
+import func from 'app/vue-temp/vue-editor-bridge'
 
 @Component({ name: 'MpMapDataV' })
 export default class MpMapDataV extends Mixins(WidgetMixin) {
+  get url() {
+    return this.widgetInfo.config.url
+  }
+
   onOpen() {
-    const page = document.getElementById('iframe_page')
-    page.requestFullscreen()
+    const page = document.getElementsByClassName('mp-widget-map-data-v')[0]
+    DomUtil.inFullScreen(page)
+    window.addEventListener('keydown', e => {
+      if (e && e.keyCode === 122) {
+        e.preventDefault()
+        e.stopPropagation()
+        DomUtil.inFullScreen(page)
+      }
+    })
   }
 }
 </script>
