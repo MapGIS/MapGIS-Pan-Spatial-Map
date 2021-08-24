f6b7cdc77f6f2f436784bcad61044cc761d841d1
龚瑞强 <1739087280@qq.com>
2021-08-24 14:42:26 +0800

    fix:修改计算地图最近级别的算法

--- packages/MapGIS-Web-App-Framework/src/utils/map-resolution-util.js
+++ packages/MapGIS-Web-App-Framework/src/utils/map-resolution-util.js
@@ -5,12 +5,41 @@
  * @returns 当前分辨对应地图里面的层级
  */
 export function getLevelInMap(resolution, levelResolutions) {
-  let level = levelResolutions.length - 1
-  for (let index = levelResolutions.length - 1; index >= 1; index--) {
-    const levelResolution = levelResolutions[index]
-    if (resolution > levelResolution.resolution) {
-      level = levelResolutions[index - 1].level
-    }
-  }
-  return level
+  // let level = levelResolutions[levelResolutions.length - 1].level
+  // for (let index = levelResolutions.length - 1; index >= 1; index--) {
+  //   const levelResolution = levelResolutions[index]
+  //   if (resolution > levelResolution.resolution) {
+  //     level = levelResolutions[index - 1].level
+  //   }
+  // }
+  const nearlevel = levelResolutions.sort(function(a, b) {
+    return (
+      Math.abs(a.resolution - resolution) - Math.abs(b.resolution - resolution)
+    )
+  })[0].level
+  return nearlevel
+}
+
+/**
+ * 通过分辨率来计算偏移量
+ * @param {number} resolution 当前层级分辨率
+ * @param {Array<Record<string,number>>} levelResolutions 当前层级与分辨率关系数组
+ * @param {number} levelValue 初始层级
+ * @param {boolean} isWGS84 是否为经纬度
+ * @returns 偏移量
+ */
+export function getZoomOffsetByResolution({
+  resolution,
+  levelResolutions,
+  levelValue,
+  isWGS84
+}) {
+  debugger
+  // 如果分辨率单位为（度/像素）需要转换为（米/像素）
+  resolution = isWGS84 ? resolution * 111194.872221777 : resolution
+  // 获取当前分辨率对应cesium里面的层级，计算偏移量
+  const level = getLevelInMap(resolution, levelResolutions)
+  const zoomOffset = levelValue - level
+
+  return zoomOffset
 }
