a19f79c81c8a3c71bbf6190f1ed812845b16344d
龚瑞强 <1739087280@qq.com>
2021-09-03 17:29:03 +0800

    feat:添加综合查询聚合功能

--- packages/pan-spatial-map-plugin-editor/src/widgets/comprehensive-query/components/PlaceName/PlaceNameCesium.vue
+++ packages/pan-spatial-map-plugin-editor/src/widgets/comprehensive-query/components/PlaceName/PlaceNameCesium.vue
@@ -9,6 +9,13 @@
       >
       </mp-3d-marker-set-pro>
     </template>
+    <!-- 聚合标注专题图 -->
+    <mapgis-3d-mapv-layer
+      v-else
+      :geojson="geojson"
+      :options="options"
+      count-field="count"
+    />
   </div>
 </template>
 
@@ -34,6 +41,28 @@ export default class PlaceNameCesium extends Mixins(MapMixin) {
   @Prop() cluster!: boolean
 
   @Prop() geojson!: Record<string, unknown>
+
+  options = {
+    fillStyle: 'rgba(255, 50, 0, 1.0)',
+    size: 50 / 3 / 2, // 非聚合点的半径
+    minSize: 8, // 聚合点最小半径
+    maxSize: 31, // 聚合点最大半径
+    globalAlpha: 0.8, // 透明度
+    clusterRadius: 150, // 聚合像素半径
+    maxClusterZoom: 18, // 最大聚合的级别
+    maxZoom: 19, // 最大显示级别
+    minPoints: 5, // 最少聚合点数，点数多于此值才会被聚合
+    extent: 400, // 聚合的细腻程度，越高聚合后点越密集
+    label: {
+      // 聚合文本样式
+      show: true, // 是否显示
+      fillStyle: 'white'
+    },
+    gradient: { 0: 'blue', 0.5: 'yellow', 1.0: 'rgb(255,0,0)' }, // 聚合图标渐变色
+    draw: 'cluster',
+    cesium: { postRender: true, postRenderFrame: 0 },
+    context: '2d'
+  }
 
   setMapCenter(positionCoord) {
     this.webGlobe.flyTo(positionCoord[0], positionCoord[1])
--- packages/pan-spatial-map-plugin-editor/src/widgets/comprehensive-query/components/PlaceName/PlaceNameMapbox.vue
+++ packages/pan-spatial-map-plugin-editor/src/widgets/comprehensive-query/components/PlaceName/PlaceNameMapbox.vue
@@ -9,6 +9,8 @@
       >
       </mp-marker-set-pro>
     </template>
+    <!-- 聚合标注专题图 -->
+    <mapgis-mapv-layer v-else :geojson="geojson" :options="options" />
   </div>
 </template>
 
@@ -34,6 +36,28 @@ export default class PlaceNameMapbox extends Mixins(MapMixin) {
   @Prop() cluster!: boolean
 
   @Prop() geojson!: Record<string, unknown>
+
+  options = {
+    fillStyle: 'rgba(255, 50, 0, 1.0)',
+    size: 50 / 3 / 2, // 非聚合点的半径
+    minSize: 8, // 聚合点最小半径
+    maxSize: 31, // 聚合点最大半径
+    globalAlpha: 0.8, // 透明度
+    clusterRadius: 150, // 聚合像素半径
+    maxClusterZoom: 18, // 最大聚合的级别
+    maxZoom: 19, // 最大显示级别
+    minPoints: 5, // 最少聚合点数，点数多于此值才会被聚合
+    extent: 400, // 聚合的细腻程度，越高聚合后点越密集
+    label: {
+      // 聚合文本样式
+      show: true, // 是否显示
+      fillStyle: 'white'
+    },
+    gradient: { 0: 'blue', 0.5: 'yellow', 1.0: 'rgb(255,0,0)' }, // 聚合图标渐变色
+    cesium: { postRender: true, postRenderFrame: 0 },
+    draw: 'cluster',
+    context: '2d'
+  }
 
   setMapCenter(positionCoord) {
     this.map.flyTo({
