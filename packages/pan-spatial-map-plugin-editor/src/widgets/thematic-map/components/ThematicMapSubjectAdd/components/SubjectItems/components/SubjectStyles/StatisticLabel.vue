a7a5cd3092257d19af0e3e17db7bc49a73e0f632
zhuxiuli <zhuxiuli.julie@mapgis.com>
2021-09-27 18:07:49 +0800

    test: 联测分段专题图

--- packages/pan-spatial-map-plugin-editor/src/widgets/thematic-map/components/ThematicMapSubjectAdd/components/SubjectItems/components/SubjectStyles/StatisticLabel.vue
+++ packages/pan-spatial-map-plugin-editor/src/widgets/thematic-map/components/ThematicMapSubjectAdd/components/SubjectItems/components/SubjectStyles/StatisticLabel.vue
@@ -1,11 +1,15 @@
 <template>
   <!-- 等级符号专题图 -->
-  <div></div>
+  <mapgis-ui-custom-panel ref="customPanelForm" :options="options" />
 </template>
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { UUID, Feature } from '@mapgis/web-app-framework'
 
 @Component
-export default class StatisticLabel extends Vue {}
+export default class StatisticLabel extends Vue {
+  get options() {
+    return []
+  }
+}
 </script>
-<style lang="less" scoped></style>
--- packages/pan-spatial-map-plugin-editor/src/widgets/thematic-map/components/ThematicMapSubjectAdd/store/dep.ts
+++ packages/pan-spatial-map-plugin-editor/src/widgets/thematic-map/components/ThematicMapSubjectAdd/store/dep.ts
@@ -1,6 +1,42 @@
+import { Feature } from '@mapgis/web-app-framework'
+
 class Dep {
   constructor(private subs: Vue[] = []) {
     this.subs = subs
+  }
+
+  /**
+   * 获取指定属性的GeoJSON数据
+   * @param {object} params 查询参数
+   */
+  async getFieldGeoJson({
+    ip,
+    port,
+    docName,
+    layerName,
+    layerIndex,
+    gdbp,
+    field
+  }) {
+    if (!field) return
+    const dataSet: Feature.FeatureIGS = await Feature.FeatureQuery.query({
+      ip,
+      port,
+      fields: field,
+      IncludeGeometry: true,
+      f: 'json',
+      ...(docName
+        ? {
+            docName,
+            layerName,
+            layerIdxs: layerIndex
+          }
+        : {
+            gdbp
+          })
+    })
+    if (!dataSet) return
+    return Feature.FeatureConvert.featureIGSToFeatureGeoJSON(dataSet)
   }
 
   getSub() {
