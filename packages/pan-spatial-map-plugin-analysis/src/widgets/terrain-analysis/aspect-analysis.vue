ad25286763006994fb20b706cd5051c3bb74386e
gongyuejian <gongyuejian.000@mapgis.com>
2021-09-29 15:38:18 +0800

    fix：三维分析中光照设置控制

--- packages/pan-spatial-map-plugin-analysis/src/widgets/terrain-analysis/aspect-analysis.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/terrain-analysis/aspect-analysis.vue
@@ -17,7 +17,7 @@
 </template>
 <script lang="ts">
 import { Vue, Component, Mixins } from 'vue-property-decorator'
-import { WidgetMixin, ColorUtil } from '@mapgis/web-app-framework'
+import { WidgetMixin, ColorUtil, Objects } from '@mapgis/web-app-framework'
 import MpColorsSetting from './components/colors-setting.vue'
 
 @Component({
@@ -36,10 +36,22 @@ export default class MpAspectAnalysis extends Mixins(WidgetMixin) {
     { min: 300, max: 360, color: 'rgba(76, 175, 80, 0.5)' }
   ]
 
-  private brightnessEnabled = false // 光照是否已开启
+  private isEnableLighting = undefined // 光照是否已开启
+
+  private noBrightness = undefined // 是否有brightness对象
+
+  private brightnessStatusAndUniformsBrightness = undefined // 光照参数
 
   private info =
     '坡向分析需要带法线地形。\r\n坡向按照东北西南的顺序表示方向,即0°表示坡向指向正东方向。'
+
+  get sceneControllerInstance() {
+    return Objects.SceneController.getInstance(
+      this.Cesium,
+      this.CesiumZondy,
+      this.webGlobe
+    )
+  }
 
   created() {
     window.AspectAnalyzeManage = {
@@ -48,12 +60,7 @@ export default class MpAspectAnalysis extends Mixins(WidgetMixin) {
     }
   }
 
-  onActive() {
-    const { viewer } = this.webGlobe
-    if (viewer.scene.globe.enableLighting && viewer.scene.brightness) {
-      this.brightnessEnabled = true
-    }
-  }
+  onActive() {}
 
   // 微件失活时
   onDeActive() {
@@ -63,17 +70,34 @@ export default class MpAspectAnalysis extends Mixins(WidgetMixin) {
   /**
    * 开启光照
    */
-  enableBrightness() {
+  enableBrightnessFuc() {
     // 开启光照，不然放大地图，分析结果显示异常
-    const { viewer } = this.webGlobe
-    viewer.scene.globe.enableLighting = true
+
+    this.isEnableLighting = this.sceneControllerInstance.isEnableLighting()
+    if (!this.isEnableLighting) {
+      // 未开启光照，开启
+      this.sceneControllerInstance.setEnableLighting(true)
+    }
     // 调高亮度
+    const { viewer } = this.webGlobe
     const stages = viewer.scene.postProcessStages
-    viewer.scene.brightness =
-      viewer.scene.brightness ||
-      stages.add(this.Cesium.PostProcessStageLibrary.createBrightnessStage())
-    viewer.scene.brightness.enabled = true
-    viewer.scene.brightness.uniforms.brightness = 1.2
+    const brightness = this.sceneControllerInstance.getBrightness()
+    if (!brightness) {
+      // 初始没有brightness对象
+      this.noBrightness = true
+      viewer.scene.brightness = stages.add(
+        this.Cesium.PostProcessStageLibrary.createBrightnessStage()
+      )
+    }
+    // 设置前记录原有光照参数
+    this.brightnessStatusAndUniformsBrightness = this.sceneControllerInstance.getBrightnessStatusAndUniformsBrightness()
+    const statusAndUniformsBrightness = {
+      enabled: true,
+      brightness: 1.2
+    }
+    this.sceneControllerInstance.setBrightnessStatusAndUniformsBrightness(
+      statusAndUniformsBrightness
+    )
   }
 
   add() {
@@ -100,7 +124,7 @@ export default class MpAspectAnalysis extends Mixins(WidgetMixin) {
       // 绘制完成回调函数
       callback: positions => {
         this.remove()
-        this.enableBrightness() // 开启光照
+        this.enableBrightnessFuc() // 开启光照
         window.AspectAnalyzeManage.aspectAnalysis =
           window.AspectAnalyzeManage.aspectAnalysis ||
           new this.Cesium.TerrainAnalyse(viewer, {
@@ -129,6 +153,38 @@ export default class MpAspectAnalysis extends Mixins(WidgetMixin) {
     return arr
   }
 
+  // 恢复光照设置
+  restoreEnableLighting() {
+    // 恢复光照开启状态设置
+    if (
+      this.isEnableLighting !== undefined &&
+      this.isEnableLighting !== this.sceneControllerInstance.isEnableLighting()
+    ) {
+      this.sceneControllerInstance.setEnableLighting(this.isEnableLighting)
+    }
+    const stages = this.webGlobe.viewer.scene.postProcessStages
+    if (this.noBrightness) {
+      // 如果开始没有brightness对象，恢复
+      stages.remove(this.webGlobe.viewer.scene.brightness)
+      this.webGlobe.viewer.scene.brightness = undefined
+    } else {
+      // 恢复brightness参数设置
+      if (this.brightnessStatusAndUniformsBrightness !== undefined) {
+        const brightnessStatusAndUniformsBrightness = this.sceneControllerInstance.getBrightnessStatusAndUniformsBrightness()
+        if (
+          this.brightnessStatusAndUniformsBrightness.enabled !==
+            brightnessStatusAndUniformsBrightness.enabled ||
+          this.brightnessStatusAndUniformsBrightness.brightness !==
+            brightnessStatusAndUniformsBrightness.brightness
+        ) {
+          this.sceneControllerInstance.setBrightnessStatusAndUniformsBrightness(
+            this.brightnessStatusAndUniformsBrightness
+          )
+        }
+      }
+    }
+  }
+
   remove() {
     // 判断是否已有坡向分析结果
     if (window.AspectAnalyzeManage.aspectAnalysis) {
@@ -143,12 +199,8 @@ export default class MpAspectAnalysis extends Mixins(WidgetMixin) {
       window.AspectAnalyzeManage.drawElement = null
     }
 
-    // 关闭光照
-    const { viewer } = this.webGlobe
-    if (viewer.scene.brightness && !this.brightnessEnabled) {
-      viewer.scene.globe.enableLighting = false
-      viewer.scene.brightness.enabled = false
-    }
+    // 恢复光照设置
+    this.restoreEnableLighting()
   }
 }
 </script>
