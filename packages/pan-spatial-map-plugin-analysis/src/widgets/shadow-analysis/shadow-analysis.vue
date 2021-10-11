aecb35b8d3db8bd518a9178bb3f86f858ded3092
gongyuejian <gongyuejian.000@mapgis.com>
2021-10-11 16:06:43 +0800

    feat：坡度分析、坡向分析、等值线分析、洪水淹没分析、填挖方分析、剖面分析、粒子特效、阴影分析、天际线分析、通视分析、可视域分析使用webclient的组件

--- packages/pan-spatial-map-plugin-analysis/src/widgets/shadow-analysis/shadow-analysis.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/shadow-analysis/shadow-analysis.vue
@@ -1,404 +1,55 @@
 <template>
-  <div class="mp-widget-shadow-analysis">
-    <mp-setting-form>
-      <a-form-item label="日期">
-        <a-date-picker
-          :default-value="formData.date | formatToMoment('YYYY-MM-DD')"
-          @change="changeDate"
-        />
-      </a-form-item>
-      <a-form-item label="开始时间">
-        <a-time-picker
-          :default-value="formData.startTime | formatToMoment"
-          @change="changeTime($event, 'startTime')"
-        />
-      </a-form-item>
-      <a-form-item label="结束时间">
-        <a-time-picker
-          :default-value="formData.endTime | formatToMoment"
-          @change="changeTime($event, 'endTime')"
-        />
-      </a-form-item>
-      <a-form-item label="底部高程">
-        <a-input v-model.number="formData.min" type="number" addon-after="米" />
-      </a-form-item>
-      <a-form-item label="拉伸高度">
-        <a-input
-          v-model.number="formData.max"
-          type="number"
-          min="0"
-          addon-after="米"
-        />
-      </a-form-item>
-      <a-form-item label="阴影颜色">
-        <MpColorPicker
-          :color.sync="formData.shadowColor"
-          :disableAlpha="true"
-          class="color-picker"
-        ></MpColorPicker>
-      </a-form-item>
-      <a-form-item label="非阴影颜色">
-        <MpColorPicker
-          :color.sync="formData.sunColor"
-          :disableAlpha="true"
-          class="color-picker"
-        ></MpColorPicker>
-      </a-form-item>
-      <!-- <a-form-item label="阴影率">
-        <a-input v-model.number="formData.ratio" type="number" disabled />
-      </a-form-item> -->
-    </mp-setting-form>
-    <div class="mp-footer-actions">
-      <a-button @click="shadow" :disabled="maskShow" type="primary">
-        分析
-      </a-button>
-      <a-button @click="sun" :disabled="maskShow" type="primary">
-        日照效果
-      </a-button>
-      <a-button @click="remove">
-        清除
-      </a-button>
-    </div>
-    <mp-mask
-      :parentDivClass="'mp-map-container'"
-      :loading="maskShow"
-      :text="maskText"
-    />
-  </div>
+  <mapgis-3d-shadow
+    :shadowColor='shadowColor'
+    :sunColor='sunColor'
+    :minHeight='minHeight'
+    :stretchHeight='stretchHeight'
+    @load='load'></mapgis-3d-shadow>
 </template>
 
 <script lang="ts">
 import { Mixins, Component } from 'vue-property-decorator'
-import { WidgetMixin, Objects } from '@mapgis/web-app-framework'
-import moment from 'moment'
+import { WidgetMixin} from '@mapgis/web-app-framework'
 
 @Component({
   name: 'MpShadowAnalysis',
-  filters: {
-    formatToMoment(value, format = 'HH:mm:ss') {
-      return moment(value, format)
-    }
-  }
 })
 export default class MpShadowAnalysis extends Mixins(WidgetMixin) {
-  private formData = {
-    date: moment().format('YYYY-MM-DD'), // 日期
-    startTime: '10:00:00', // 开始时间
-    endTime: '14:00:00', // 结束时间
-    min: 0, // 最低高程(米)
-    max: 20, // 拉伸高度(米)
-    shadowColor: 'rgb(0,255,0)', // 阴影颜色
-    sunColor: 'rgb(255,0,0)' // 非阴影颜色
-    // ratio: 0 // 阴影率(时间点范围阴影分析输出结果)
-  }
 
-  private percent = 0
+  // 阴影颜色
+  private shadowColor = 'rgba(0,255,0,255)'
 
-  private maskShow = false
+  // 非阴影颜色
+  private sunColor = 'rgba(255,0,0,255)'
 
-  private maskText = '正在分析中, 请稍等...0%'
+  // 最低高程(米)
+  private minHeight = 0
 
-  get julianStartDate() {
-    return this.getJulianDate(
-      `${this.formData.date} ${this.formData.startTime}`
-    )
-  }
+  // 拉伸高度(米)
+  private stretchHeight = 20
 
-  get julianEndDate() {
-    return this.getJulianDate(`${this.formData.date} ${this.formData.endTime}`)
-  }
-
-  created() {
-    this.initShadowManage()
-  }
-
-  beforeDestroy() {
-    this.removeShadowManage()
-  }
-
-  /**
-   * 初始化阴影
-   */
-  initShadowManage() {
-    if (!window.ShadowManage) {
-      window.ShadowManage = {
-        drawElement: null,
-        shadowAnalysis: null
-      }
-    }
-  }
-
-  /**
-   * 移除所有
-   */
-  removeShadowManage() {
-    this.remove()
-    window.ShadowManage = undefined
-  }
+  // 阴影分析对象
+  private shadowAnalysis = null
 
   /**
    * 微件打开时
    */
   onOpen() {
-    this.initShadowManage()
+    this.shadowAnalysis.mount();
   }
 
   /**
    * 微件关闭时
    */
   onClose() {
-    this.removeShadowManage()
+    this.shadowAnalysis.removeAll();
   }
 
-  /**
-   * 微件失活时
-   */
-  // onDeActive() {
-  //   this.removeShadowManage()
-  // }
-
-  /**
-   * 时间格式化
-   */
-  formatDateTime(value, type = 'date') {
-    return moment(value).format(type === 'date' ? 'YYYY-MM-DD' : 'HH:mm:ss')
-  }
-
-  /**
-   * 日期组件值变化
-   */
-  changeDate(val) {
-    this.$set(this.formData, 'date', this.formatDateTime(val))
-  }
-
-  /**
-   * 时间组件值变化
-   */
-  changeTime(val, tag) {
-    this.$set(this.formData, tag, this.formatDateTime(val, 'time'))
-  }
-
-  /**
-   * 时间字符串转JulianDate时间
-   */
-  getJulianDate(timeStr: string) {
-    const utc = this.Cesium.JulianDate.fromDate(new Date(timeStr)) // UTC
-    return this.Cesium.JulianDate.addHours(utc, 0, new this.Cesium.JulianDate()) // 北京时间
-  }
-
-  /**
-   * 获取经纬度范围
-   */
-  getRectBounds(positions) {
-    let xmin
-    let ymin
-    let xmax
-    let ymax
-    positions.forEach(point => {
-      const { x, y } = point
-      if (xmin === undefined || x < xmin) {
-        xmin = x
-      }
-      if (xmax === undefined || x > xmax) {
-        xmax = x
-      }
-      if (ymin === undefined || y < ymin) {
-        ymin = y
-      }
-      if (ymax === undefined || y > ymax) {
-        ymax = y
-      }
-    })
-
-    return {
-      xmin,
-      ymin,
-      xmax,
-      ymax
-    }
-  }
-
-  /**
-   * 获取xyz轴阴影三维点的数量
-   */
-  getPaneNums(positions) {
-    const { min, max } = this.formData
-    const { xmin, ymin, xmax, ymax } = this.getRectBounds(positions)
-    // 多边形x方向长度
-    const recXLength = this.Cesium.Cartesian3.distance(
-      new this.Cesium.Cartesian3(xmin, ymin, 0),
-      new this.Cesium.Cartesian3(xmax, ymin, 0)
-    )
-    // 多边形y方向长度
-    const recYLength = this.Cesium.Cartesian3.distance(
-      new this.Cesium.Cartesian3(xmin, ymin, 0),
-      new this.Cesium.Cartesian3(xmin, ymax, 0)
-    )
-    const xPaneNum = Math.ceil(recXLength / 4) // X轴方向插值点个数
-    const yPaneNum = Math.ceil(recYLength / 4) // Y轴方向插值点个数
-    const zPaneNum = Math.ceil((max - min) / 4) // Z轴方向插值点个数
-
-    return {
-      xPaneNum,
-      yPaneNum,
-      zPaneNum
-    }
-  }
-
-  /**
-   * 阴影分析遮罩层
-   */
-  toggleMask(status: boolean) {
-    this.maskShow = status
-  }
-
-  /**
-   * 时间段阴影分析回调函数，获取分析进度值
-   */
-  setPercent(result) {
-    this.percent = result
-    this.maskText = `正在分析中, 请稍等...${Number((result * 100).toFixed(2))}%`
-    const timer = setInterval(() => {
-      if (this.percent === result) {
-        this.toggleMask(false)
-      }
-      clearInterval(timer)
-    }, 200)
-  }
-
-  /**
-   * 时间点阴影分析回调函数，获取阴影率
-   */
-  // setShadowRatio(result) {
-  //   this.$set(this.formData, 'ratio', result)
-  // }
-
-  /**
-   * 范围时间点阴影分析/范围时间段阴影分析
-   */
-  shadow() {
-    this.removeSun()
-    this.removeShadow()
-    const { viewer } = this.webGlobe
-    // 初始化交互式绘制控件
-    if (!window.ShadowManage.drawElement) {
-      window.ShadowManage.drawElement = new this.Cesium.DrawElement(viewer)
-    }
-    // 激活交互式绘制工具
-    window.ShadowManage.drawElement.startDrawingPolygon({
-      // 绘制完成回调函数
-      callback: positions => {
-        this.removeDraw()
-        this.toggleMask(true)
-        const {
-          date,
-          min,
-          max,
-          shadowColor,
-          sunColor,
-          startTime,
-          endTime
-        } = this.formData
-        if (!window.ShadowManage.shadowAnalysis) {
-          // 初始化取阴影分析类
-          window.ShadowManage.shadowAnalysis = new this.Cesium.ShadowAnalysis(
-            viewer,
-            {
-              ...this.getPaneNums(positions),
-              shadowColor,
-              sunColor,
-              percentCallback: this.setPercent
-              // shadowRatioCallBack: this.setShadowRatio,
-            }
-          )
-        }
-        viewer.clockViewModel.currentTime = this.julianStartDate
-        // 阴影分析
-        window.ShadowManage.shadowAnalysis.calcPointsArrayInShadowTime(
-          positions,
-          min,
-          max,
-          new Date(`${date} ${startTime}`),
-          new Date(`${date} ${endTime}`)
-        )
-      }
-    })
-  }
-
-  /**
-   * 原生日照分析
-   */
-  sun() {
-    this.removeSun()
-    const { viewer } = this.webGlobe
-    viewer.scene.globe.enableLighting = true // 开启日照
-    viewer.shadows = true // 开启阴影
-    viewer.clock.shouldAnimate = true // 开启计时
-    viewer.clock.currentTime = this.julianStartDate
-    viewer.clock.startTime = this.julianStartDate
-    viewer.clock.stopTime = this.julianEndDate
-    viewer.clock.multiplier = 3600 // cesium中1秒表示现实中1个小时
-    viewer.clock.clockRange = this.Cesium.ClockRange.LOOP_STOP // 循环动画
-  }
-
-  /**
-   * 移除绘制分析结果
-   */
-  removeDraw() {
-    if (window.ShadowManage?.drawElement) {
-      // 取消交互式绘制矩形事件激活状态
-      window.ShadowManage.drawElement.stopDrawing()
-      window.ShadowManage.drawElement = null
-    }
-  }
-
-  /**
-   * 移除日照分析结果
-   */
-  removeSun() {
-    const { viewer } = this.webGlobe
-    viewer.scene.globe.enableLighting = false
-    viewer.shadows = false
-    viewer.clock.multiplier = 1
-    viewer.clock.shouldAnimate = false // 关闭计时
-  }
-
-  /**
-   * 移除阴影分析结果
-   */
-  removeShadow() {
-    // 判断是否已有阴影分析结果
-    if (window.ShadowManage?.shadowAnalysis) {
-      // 移除阴影分析显示结果
-      window.ShadowManage.shadowAnalysis.remove()
-      window.ShadowManage.shadowAnalysis = null
-    }
-  }
-
-  /**
-   * 清除
-   */
-  remove() {
-    this.removeDraw()
-    this.removeShadow()
-    this.removeSun()
-    this.toggleMask(false)
-    // this.setShadowRatio(0)
+  load(shadowAnalysis){
+      this.shadowAnalysis = shadowAnalysis;
   }
 }
 </script>
 
 <style lang="less" scoped>
-@import '../index.less';
-
-::v-deep {
-  .ant-calendar-picker,
-  .ant-time-picker {
-    width: 100%;
-  }
-}
-.mp-widget-shadow-analysis {
-  display: flex;
-  flex-direction: column;
-}
 </style>
--- packages/pan-spatial-map-plugin-analysis/src/widgets/visibility-analysis/visibility-analysis.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/visibility-analysis/visibility-analysis.vue
@@ -1,336 +1,42 @@
 <template>
-  <div class="mp-widget-visibility-analysis">
-    <div class="visibility-panel">
-      <mp-setting-form v-model="formData">
-        <a-form-item label="附加高度(米)">
-          <a-input
-            v-model.number="formData.exHeight"
-            type="number"
-            :min="0"
-            :step="0.1"
-          />
-        </a-form-item>
-        <a-form-item label="不可视区域颜色">
-          <mp-color-picker
-            :disableAlpha="false"
-            :color="formData.unVisibleColor"
-            @input="
-              val =>
-                (formData.unVisibleColor = `rgba(${val.rgba.r}, ${val.rgba.g}, ${val.rgba.b}, ${val.rgba.a})`)
-            "
-          ></mp-color-picker>
-        </a-form-item>
-        <a-form-item label="可视区域颜色">
-          <mp-color-picker
-            :disableAlpha="false"
-            :color="formData.visibleColor"
-            @input="
-              val =>
-                (formData.visibleColor = `rgba(${val.rgba.r}, ${val.rgba.g}, ${val.rgba.b}, ${val.rgba.a})`)
-            "
-          ></mp-color-picker>
-        </a-form-item>
-      </mp-setting-form>
-    </div>
-    <div class="mp-footer-actions">
-      <a-button type="primary" @click="onClickStart">分析</a-button>
-      <a-button @click="onClickStop">清除</a-button>
-    </div>
-  </div>
+  <mapgis-3d-sightline
+    :exHeight='exHeight'
+    :visibleColor='visibleColor'
+    :unVisibleColor='unVisibleColor'
+    @load='load'>
+  </mapgis-3d-sightline>
 </template>
 
-<script lang="ts">
-import { Mixins, Component, Watch } from 'vue-property-decorator'
+<script lang='ts'>
+import { Mixins, Component } from 'vue-property-decorator'
 import {
-  WidgetMixin,
-  LayerType,
-  IGSSceneSublayerRenderType,
-  Objects
+  WidgetMixin
 } from '@mapgis/web-app-framework'
 
 @Component({ name: 'MpVisibilityAnalysis' })
 export default class MpVisibilityAnalysis extends Mixins(WidgetMixin) {
-  private formData = {
-    exHeight: 1.85,
-    visibleColor: '#008000',
-    unVisibleColor: '#ff0000'
-  }
 
-  // 是否为鼠标注册了监听事件
-  private isAddEventListener = false
+  // 附加高度(米)
+  private exHeight = 1.85
 
-  // 是否已选择观察点位置
-  private hasViewPosition = false
+  // 可视区域颜色
+  private visibleColor = '#008000'
 
-  // 观察点
-  private viewPoint
+  // 不可视区域颜色
+  private unVisibleColor = '#ff0000'
 
-  // 目标点
-  private targetPoint
-
-  // 观察点坐标
-  private viewPosition
-
-  // 深度检测是否已开启，默认为undefined，当这个值为undefined的时候，说明没有赋值，不做任何处理
-  private isDepthTestAgainstTerrainEnable = undefined
-
-  get sceneControllerInstance() {
-    return Objects.SceneController.getInstance(
-      this.Cesium,
-      this.CesiumZondy,
-      this.webGlobe
-    )
-  }
-
-  get formDataClone() {
-    return JSON.parse(JSON.stringify(this.formData))
-  }
-
-  @Watch('formDataClone', { deep: true })
-  onColorChange(newVal, oldVal) {
-    const unVisibleColor = new this.Cesium.Color.fromCssColorString(
-      newVal.unVisibleColor
-    )
-    const visibleColor = new this.Cesium.Color.fromCssColorString(
-      newVal.visibleColor
-    )
-    if (this.visibilityArr.length > 0) {
-      this.visibilityArr.forEach(item => {
-        item._unvisibleColor = unVisibleColor
-        item._visibleColor = visibleColor
-        if (newVal.exHeight !== oldVal.exHeight) {
-          // 改变通视分析工具的附加高度(分析工具的观察点坐标也会同时更新)
-          item.exHeight = newVal.exHeight - oldVal.exHeight
-
-          // 改变观察点坐标
-          this.viewPoint.position._value = item.viewPosition
-          // 记录新的观察点坐标
-          this.viewPosition = item.viewPosition
-        }
-      })
-    }
-  }
-
-  created() {
-    this.initData()
-  }
+  // 通视分析对象
+  private visibilityAnalysis = null
 
   onClose() {
-    this.onClickStop()
+    this.visibilityAnalysis.unmount()
   }
 
-  // 初始化各项数据
-  private initData() {
-    window.VisibilityAnalysisManage = {
-      visibility: null
-    }
-
-    // 通视分析结果集
-    this.visibilityArr = []
-  }
-
-  // 创建通视分析工具
-  private createVisibility() {
-    const unVisibleColor = new this.Cesium.Color.fromCssColorString(
-      this.formData.unVisibleColor
-    )
-    const visibleColor = new this.Cesium.Color.fromCssColorString(
-      this.formData.visibleColor
-    )
-
-    // 初始化高级分析功能管理类
-    const advancedAnalysisManager = new window.CesiumZondy.Manager.AdvancedAnalysisManager(
-      {
-        viewer: this.webGlobe.viewer
-      }
-    )
-
-    // 初始化通视分析类
-    const visibility = advancedAnalysisManager.createVisibilityAnalysis()
-    visibility._unvisibleColor = unVisibleColor
-    visibility._visibleColor = visibleColor
-
-    this.visibilityArr.push(visibility)
-
-    return visibility
-  }
-
-  // 是否开启地形深度检测
-  private isOpenDepthTest() {
-    // 若分析的模型是三维地形，则开启地形深度检测,否则关闭深度检测。若未选择任何模型，则默认开启深度检测
-    let needDepthTestAgainstTerrain = false // 是否需要开启深度检测
-    const doc: Document = this.document
-    if (doc.defaultMap && doc.defaultMap.allLayers.length > 0) {
-      const analysisLry =
-        doc.defaultMap.allLayers[doc.defaultMap.allLayers.length - 1]
-
-      // 首先判断勾选的是否是三维图层
-      if (analysisLry.type === LayerType.IGSScene) {
-        // 然后判断是否是三维地形
-        if (
-          analysisLry.activeScene.sublayers.some(item => {
-            return item.renderType === IGSSceneSublayerRenderType.elevation
-          })
-        ) {
-          // 开启地形深度测试
-          needDepthTestAgainstTerrain = true
-        } else {
-          needDepthTestAgainstTerrain = false
-        }
-      } else {
-        needDepthTestAgainstTerrain = true
-      }
-    } else {
-      needDepthTestAgainstTerrain = true
-    }
-    if (needDepthTestAgainstTerrain) {
-      this.isDepthTestAgainstTerrainEnable = this.sceneControllerInstance.isDepthTestAgainstTerrainEnable()
-      if (!this.isDepthTestAgainstTerrainEnable) {
-        // 如果深度检测没有开启，则开启
-        this.sceneControllerInstance.setDepthTestAgainstTerrainEnable(true)
-      }
-    }
-  }
-
-  // 点击“分析”按钮回调
-  private onClickStart() {
-    this.onClickStop()
-
-    this.isOpenDepthTest()
-
-    this.addEventListener()
-  }
-
-  // 点击“结束分析”按钮回调
-  private onClickStop() {
-    // 注销鼠标的各项监听事件
-    this.webGlobe.unRegisterMouseEvent('LEFT_CLICK')
-    this.webGlobe.unRegisterMouseEvent('RIGHT_CLICK')
-
-    this.webGlobe.viewer.entities.removeAll()
-
-    if (this.visibilityArr.length > 0) {
-      this.visibilityArr.forEach(item => {
-        // 移除通视分析结果
-        this.webGlobe.viewer.scene.VisualAnalysisManager.remove(item)
-        // 销毁通视分析类
-        item.destroy()
-      })
-    }
-
-    this.restoreDepthTestAgainstTerrain()
-    this.hasViewPosition = false
-    this.isAddEventListener = false
-    this.visibilityArr = []
-  }
-
-  restoreDepthTestAgainstTerrain() {
-    // 恢复深度检测设置
-    if (
-      this.isDepthTestAgainstTerrainEnable !== undefined &&
-      this.isDepthTestAgainstTerrainEnable !==
-        this.sceneControllerInstance.isDepthTestAgainstTerrainEnable()
-    ) {
-      this.sceneControllerInstance.setDepthTestAgainstTerrainEnable(
-        this.isDepthTestAgainstTerrainEnable
-      )
-    }
-  }
-
-  // 为鼠标的各种行为注册监听事件
-  private addEventListener() {
-    if (!this.isAddEventListener) {
-      this.webGlobe.registerMouseEvent('LEFT_CLICK', event => {
-        this.registerMouseLClickEvent(event)
-      })
-      this.webGlobe.registerMouseEvent('RIGHT_CLICK', event => {
-        this.registerMouseRClickEvent(event)
-      })
-
-      this.isAddEventListener = true
-    }
-  }
-
-  // 注册通视分析鼠标左键点击事件
-  private registerMouseLClickEvent(event) {
-    let cartesian = this.webGlobe.viewer.getCartesian3Position(event.position)
-
-    if (!this.hasViewPosition && cartesian !== undefined) {
-      // 若还未选择观察点,则记录下观察点坐标
-
-      // 获取当前坐标系标准
-      const ellipsoid = this.webGlobe.viewer.scene.globe.ellipsoid
-      // 根据坐标系标准，将笛卡尔坐标转换为地理坐标
-      const cartographic = ellipsoid.cartesianToCartographic(cartesian)
-      // 抬高观察点
-      cartographic.height += this.formData.exHeight
-
-      cartesian = this.Cesium.Cartographic.toCartesian(cartographic)
-      this.viewPosition = cartesian
-
-      // 添加观察点到地图
-      this.addViewPoint(cartesian)
-      this.hasViewPosition = true
-    } else {
-      const visibility = this.createVisibility()
-
-      // 设置通视分析观察点坐标
-      visibility.viewPosition = this.viewPosition
-
-      // 设置通视分析结束点坐标
-      visibility.targetPosition = cartesian
-
-      // 添加目标点到地图
-      this.addTargetPoint(cartesian)
-    }
-  }
-
-  // 注册通视分析鼠标右键点击事件
-  private registerMouseRClickEvent(event) {
-    // 注销鼠标的各项监听事件
-    this.webGlobe.unRegisterMouseEvent('LEFT_CLICK')
-    this.webGlobe.unRegisterMouseEvent('RIGHT_CLICK')
-    this.isAddEventListener = false
-  }
-
-  // 添加观察点到地图上
-  private addViewPoint(cartesian) {
-    this.removeViewPoint()
-
-    this.viewPoint = this.webGlobe.viewer.entities.add({
-      position: cartesian,
-      point: {
-        color: this.Cesium.Color.BLUE,
-        pixelSize: 10
-      }
-    })
-  }
-
-  // 添加目标点到地图上
-  private addTargetPoint(cartesian) {
-    this.removeTargetPoint()
-
-    this.targetPoint = this.webGlobe.viewer.entities.add({
-      position: cartesian,
-      point: {
-        color: this.Cesium.Color.RED,
-        pixelSize: 10
-      }
-    })
-  }
-
-  // 从地图上移除观察点
-  private removeViewPoint() {
-    if (this.viewPoint) this.webGlobe.viewer.entities.remove(this.viewPoint)
-  }
-
-  // 从地图上移除目标点
-  private removeTargetPoint() {
-    if (this.targetPoint) this.webGlobe.viewer.entities.remove(this.targetPoint)
+  load(visibilityAnalysis) {
+    this.visibilityAnalysis = visibilityAnalysis
   }
 }
 </script>
 
-<style lang="less" scoped>
-@import '../index.less';
+<style lang='less' scoped>
 </style>
