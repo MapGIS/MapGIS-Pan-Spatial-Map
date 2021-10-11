d12dd7ff504b1f766b299da3428e7f1d2f7a3765
龚瑞强 <1739087280@qq.com>
2021-10-11 16:30:33 +0800

    更新ceisum库，并添加三维标注图层

--- packages/pan-spatial-map-plugin-editor/src/widgets/comprehensive-query/components/PlaceName/PlaceNameCesium.vue
+++ packages/pan-spatial-map-plugin-editor/src/widgets/comprehensive-query/components/PlaceName/PlaceNameCesium.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
-    <template v-if="!cluster">
-    </template>
+    <mapgis-3d-dynamic-marker-layer
+      v-if="!cluster"
+      :data="geojson"
+      :selects="hoverMarker"
+      :highlight="false"
+      :layerStyle="layerStyle"
+      :highlightStyle="highlightStyle"
+      idField="markerId"
+    />
     <!-- 聚合标注专题图 -->
     <mapgis-3d-mapv-layer
-      v-else-if="geojson && geojson.features && geojson.features.length > 0"
+      v-else-if="
+        geojson &&
+          geojson.features &&
+          geojson.features.length > 0 &&
+          colorCluster
+      "
       :geojson="geojson"
       :options="options"
       count-field="count"
@@ -14,38 +26,91 @@
 
 <script lang="ts">
 import { Mixins, Component, Prop, Watch } from 'vue-property-decorator'
+import { Style } from '@mapgis/webclient-es6-service'
 import { MapMixin } from '@mapgis/web-app-framework'
+
+const { MarkerStyle, LineStyle, PointStyle, FillStyle } = Style
 
 @Component({ components: {} })
 export default class PlaceNameCesium extends Mixins(MapMixin) {
-  @Prop() selectedMarkerIcon!: string
+  @Prop({
+    type: String,
+    default: ''
+  })
+  selectedMarkerIcon!: string
 
-  @Prop() defaultMarkerIcon!: string
+  @Prop({
+    type: String,
+    default: ''
+  })
+  defaultMarkerIcon!: string
 
-  @Prop() cluster!: boolean
+  @Prop({
+    type: Boolean,
+    default: false
+  })
+  cluster!: boolean
 
-  @Prop() geojson!: Record<string, unknown>
+  @Prop({
+    type: String,
+    default: ''
+  })
+  colorCluster?: string
 
-  options = {
-    fillStyle: 'rgba(255, 50, 0, 1.0)',
-    size: 50 / 3 / 2, // 非聚合点的半径
-    minSize: 8, // 聚合点最小半径
-    maxSize: 31, // 聚合点最大半径
-    globalAlpha: 0.8, // 透明度
-    clusterRadius: 150, // 聚合像素半径
-    maxClusterZoom: 18, // 最大聚合的级别
-    maxZoom: 19, // 最大显示级别
-    minPoints: 5, // 最少聚合点数，点数多于此值才会被聚合
-    extent: 400, // 聚合的细腻程度，越高聚合后点越密集
-    label: {
-      // 聚合文本样式
-      show: true, // 是否显示
-      fillStyle: 'white'
-    },
-    gradient: { 0: 'blue', 0.5: 'yellow', 1.0: 'rgb(255,0,0)' }, // 聚合图标渐变色
-    draw: 'cluster',
-    cesium: { postRender: true, postRenderFrame: 0 },
-    context: '2d'
+  @Prop({
+    type: Object,
+    default: () => ({})
+  })
+  geojson!: Record<string, unknown>
+
+  @Prop({
+    type: Array,
+    default: () => []
+  })
+  hoverMarker?: Array<string>
+
+  get layerStyle() {
+    return {
+      type: 'dynamic-marker',
+      marker: new MarkerStyle({
+        symbol: this.defaultMarkerIcon
+      })
+    }
+  }
+
+  get highlightStyle() {
+    return {
+      marker: new MarkerStyle({
+        symbol: this.selectedMarkerIcon
+      }),
+      point: new PointStyle(),
+      line: new LineStyle(),
+      polygon: new FillStyle()
+    }
+  }
+
+  get options() {
+    return {
+      fillStyle: this.colorCluster,
+      size: 50 / 3 / 2, // 非聚合点的半径
+      minSize: 8, // 聚合点最小半径
+      maxSize: 31, // 聚合点最大半径
+      globalAlpha: 0.8, // 透明度
+      clusterRadius: 150, // 聚合像素半径
+      maxClusterZoom: 18, // 最大聚合的级别
+      maxZoom: 19, // 最大显示级别
+      minPoints: 5, // 最少聚合点数，点数多于此值才会被聚合
+      extent: 400, // 聚合的细腻程度，越高聚合后点越密集
+      label: {
+        // 聚合文本样式
+        show: true, // 是否显示
+        fillStyle: 'white'
+      },
+      gradient: { 0: 'blue', 0.5: 'yellow', 1.0: 'rgb(255,0,0)' }, // 聚合图标渐变色
+      cesium: { postRender: true, postRenderFrame: 0 },
+      draw: 'cluster',
+      context: '2d'
+    }
   }
 }
 </script>
