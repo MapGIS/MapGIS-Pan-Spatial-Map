7eeb43c791bda82708641f0d6cc36c50b42ff42f
gongyuejian <gongyuejian.000@mapgis.com>
2021-11-25 11:55:32 +0800

    fix：添加数据上传文件的bug修复

--- packages/pan-spatial-map-plugin-editor/src/widgets/add-data/components/AddDataFile.vue
+++ packages/pan-spatial-map-plugin-editor/src/widgets/add-data/components/AddDataFile.vue
@@ -163,7 +163,7 @@ export default class AddDataFile extends Mixins(WidgetMixin) {
       let path = ''
       this.isDisabled = false
       console.log(info)
-      if (info.file.type.indexOf('zip') !== -1) {
+      if (info.file.name.endsWith('.zip')) {
         // 上传的是zip压缩包(即shp类型文件)
         const shpItem = info.file.response.data.find(item =>
           item.url.endsWith('shp')
