119dce48ef7df65b9cb74a19cd350dd47f95df9e
zhangying-zy618 <1057936309@qq.com>
2021-12-04 21:17:40 +0800

    【修改控高分析微件】

--- packages/pan-spatial-map-plugin-analysis/src/widgets/height-limited-analysis/height-limited-analysis.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/height-limited-analysis/height-limited-analysis.vue
@@ -1,7 +1,10 @@
 <template>
   <mapgis-3d-heightlimited
-    :models="models"
     :color="color"
+    :heightLimit='heightLimit'
+    :maxSliderHeight='maxSliderHeight'
+    :minSliderHeight='minSliderHeight'
+    :drawStyle='drawStyle'
      @load='load'>
   </mapgis-3d-heightlimited>
 </template>
@@ -9,52 +12,29 @@
 <script lang="ts">
 import { Mixins, Component, Watch } from 'vue-property-decorator'
 import {
-  WidgetMixin,
-  LayerType,
-  IGSSceneSublayerRenderType,
-  LoadStatus,
-  Objects
+  WidgetMixin
 } from '@mapgis/web-app-framework'
 
 @Component({
   name: 'MpHeightLimitedAnalysis'
 })
 export default class MpHeightLimitedAnalysis extends Mixins(WidgetMixin) {
-  // 模型集合
-  private models = []
 
-  private color ="#1C9C80"
+  private color ="rgba(255,140,0,0.6)"
+
+  private heightLimit = 80
+
+  private maxSliderHeight = 180
+
+  private minSliderHeight = 0
+
+  private drawStyle = {
+    color: "#FF0000",
+    opacity: 0.6
+  }
 
   // 控高分析对象
   private heightLimitedAnalysis = undefined
-
-  /**
-   * 动态获取基础目录树上已勾选的三维模型数据
-   */
-  @Watch('document', { deep: true, immediate: true })
-  getScenes() {
-    if (!this.document) return
-    const layers = []
-    this.document.defaultMap
-      .clone()
-      .getFlatLayers()
-      .forEach((layer, index) => {
-        if (layer.loadStatus === LoadStatus.loaded) {
-          if (layer.type === LayerType.IGSScene) {
-            const { renderType } = layer.activeScene.sublayers[0]
-            if (renderType === IGSSceneSublayerRenderType.modelCache) {
-              const { id } = layer.activeScene.sublayers[0]
-              // 限高分析暂时只支持模型
-              layers.push({
-                title: layer.title,
-                vueIndex: id,
-              })
-            }
-          }
-        }
-      })
-    this.models = layers
-  }
 
   onActive() {
     this.heightLimitedAnalysis.mount()
