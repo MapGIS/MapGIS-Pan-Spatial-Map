159715516c80a89ed4a9bd99ad7ebfd89060231e
killer1992 <killer1992@yeah.net>
2021-12-16 09:34:27 +0800

    【杨琨】【添加地图故事】

---
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/map-story/map-story.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id='mapStory' class='mp-widget-map-story'>
+    <mapgis-3d-map-story-layer
+      v-if='showMapStory'
+      @projectPreview='projectPreview'
+      @featurePreview='featurePreview'
+      :height='height'
+      :width='width'
+      :enableClose='enableClose'
+      :dataSource='dataSource' />
+    <mp-window-wrapper :visible="showPreview">
+      <mp-window
+        :visible.sync="showPreview"
+        title="地图故事"
+        :is-full-screen="false"
+        :shrinkAction="false"
+        :fullScreenAction="false"
+        :horizontal-offset="48"
+        :vertical-offset="50"
+        :width="340"
+        :has-padding="false"
+        anchor="top-right"
+      >
+        <template>
+          <mapgis-3d-preview-map-story-layer
+            v-show='showPreview'
+            :height='previewHeight'
+            :width='previewWidth'
+            :dataSource='storyDataSource'
+            :enableFullScreen='enableFullScreen'
+            :enableArrow='enableArrow'
+            :enablePlay='enablePlay'
+            ref='preview'
+          />
+        </template>
+      </mp-window>
+    </mp-window-wrapper>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Mixins, Component } from 'vue-property-decorator'
+import {
+  WidgetMixin
+} from '@mapgis/web-app-framework'
+
+@Component({
+  name: 'MpMapStory',
+})
+export default class MpMapStory extends Mixins(WidgetMixin) {
+  private dataSource = [{
+    "title": "时空演变",
+    "description": "",
+    "uuid": "100001",
+    "map": {
+      "type": "WMTS",
+      "baseUrl": "http://t4.tianditu.gov.cn/cta_w/wmts?tk=9c157e9585486c02edf817d2ecbc7752",
+      "layer": "cta",
+      "tilingScheme": "EPSG:3857",
+      "tileMatrixSet": "w",
+      "format": "tiles",
+      "vueKey": "default",
+      "vueIndex": 1000213213121
+    },
+    "features": [],
+    "chapters": [{
+      "projectUUID": "100001",
+      "uuid": "111",
+      "camera": {
+        "uuid": "111"
+      },
+      "features": [],
+      "title": "章节一",
+      "content": "",
+      "animationTime": "5000"
+    }]
+  }]
+
+  private width = 263
+
+  private height = 833
+
+  private previewHeight = 770
+
+  private previewWidth = 334
+
+  private showPreview = false
+
+  private showMapStory = false
+
+  private enableFullScreen = false
+
+  private enableArrow = false
+
+  private enablePlay = false
+
+  private enableClose = false
+
+  private storyDataSource = {}
+
+  mounted() {}
+
+  onOpen() {
+    this.showMapStory = true;
+  }
+
+  featurePreview(features) {
+    this.showPreview = true;
+    this.enableArrow = false;
+    this.enablePlay = false;
+    this.storyDataSource = {
+      chapters: features
+    };
+  }
+
+  projectPreview(dataSource, enableFullScreen) {
+    this.storyDataSource = dataSource;
+    this.showPreview = true;
+    this.enableArrow = true;
+    this.enablePlay = true;
+    this.enableFullScreen = enableFullScreen;
+    this.$refs.preview.projectPreview();
+  }
+}
+</script>
+
+<style>
+.mp-widget-map-story {
+  position: absolute;
+  top: -4px;
+  left: -4px;
+}
+
+.cesium-popup-close-button {
+  margin-top: 7px;
+  margin-right: 6px;
+  font-size: 32px!important;
+}
+</style>
