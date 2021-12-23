b7954e1ae5d9e2f24212344b02db3027937b1129
gongyuejian <gongyuejian.000@mapgis.com>
2021-12-23 09:34:12 +0800

    factor：视频投放添加相机模型

--- packages/pan-spatial-map-plugin-analysis/src/widgets/video/video.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/video/video.vue
@@ -1,8 +1,11 @@
 <template>
   <mapgis-3d-video-manager
+    class="video-manager"
     :videoOverlayLayerList="videoOverlayLayerList"
     @load="load"
     @update-config="updateConfig"
+    :modelUrl="modelUrl"
+    :modelOffset="modelOffset"
   >
   </mapgis-3d-video-manager>
 </template>
@@ -15,82 +18,149 @@ import { api } from '@mapgis/pan-spatial-map-common'
   name: 'MpVideo'
 })
 export default class MpVideo extends Mixins(WidgetMixin) {
+  private modelUrl = './CesiumModels/Cesium_Camera.glb'
+
+  private modelOffset = { headingOffset: -90, pitchOffset: 0, rollOffset: 0 }
+
   private videoLayerList = [
     {
-      //  视频投放图层
       id: '123-345-567-789',
       name: 'test',
       videoList: [
-        // 视频列表
         {
-          id: '987-765-543-321', // 视频id
-          name: 'testVideo1', // 视频名称
-          description: '', // 描述
-          isProjected: false, // 是否开启视频投放
+          id: '987-765-543-321',
+          name: 'testVideo1',
+          description: '',
+          isProjected: false,
           params: {
             videoSource: {
-              protocol: 'mp4', // 视频传输协议
-              videoUrl: 'http://localhost:8080/video/video1.mp4' // 视频服务地址
+              protocol: 'm3u8',
+              videoUrl:
+                'http://192.168.91.123:10008/record/video1/20211221/out.m3u8'
             },
-            cameraPosition: { x: 0, y: 0, z: 0 }, // 相机位置
+            cameraPosition: {
+              x: 114.401228136856,
+              y: 30.467421377675457,
+              z: 84.94989410478892
+            },
             orientation: {
-              heading: 0, // 方向角
-              pitch: 0, // 俯仰角
-              roll: 0 // 滚动角
+              heading: 6.053866507322313,
+              pitch: -73.6,
+              roll: 354.1
             },
-            hFOV: 15, // 水平视场角
-            vFOV: 15, // 垂直视场角
-            hintLineVisible: true // 是否显示投放区域线
+            hFOV: 34.6,
+            vFOV: 18.9,
+            hintLineVisible: true
           }
         },
         {
-          id: '987-765-543-123', // 视频id
-          name: 'testVideo2', // 视频名称
-          description: '', // 描述
-          isProjected: false, // 是否开启视频投放
+          id: '987-765-543-123',
+          name: 'testVideo2',
+          description: '',
+          isProjected: false,
           params: {
             videoSource: {
-              protocol: 'mp4', // 视频传输协议
-              videoUrl: 'http://localhost:8080/video/video2.mp4' // 视频服务地址
+              protocol: 'm3u8',
+              videoUrl:
+                'http://192.168.91.123:10008/record/video2/20211221/out.m3u8'
             },
-            cameraPosition: { x: 0, y: 0, z: 0 }, // 相机位置
+            cameraPosition: {
+              x: 114.40088870656619,
+              y: 30.467421563975016,
+              z: 84.91172691510191
+            },
             orientation: {
-              heading: 0, // 方向角
-              pitch: 0, // 俯仰角
-              roll: 0 // 滚动角
+              heading: 359.89407747239846,
+              pitch: -74.2,
+              roll: 0
             },
-            hFOV: 15, // 水平视场角
-            vFOV: 15, // 垂直视场角
-            hintLineVisible: true // 是否显示投放区域线
+            hFOV: 33.1,
+            vFOV: 19.2,
+            hintLineVisible: true
+          }
+        },
+        {
+          id: '987-765-543-124',
+          name: 'testVideo3',
+          description: '',
+          isProjected: false,
+          params: {
+            videoSource: {
+              protocol: 'm3u8',
+              videoUrl:
+                'http://192.168.91.123:10008/record/video3/20211221/out.m3u8'
+            },
+            cameraPosition: {
+              x: 114.4006886798949,
+              y: 30.467287432107295,
+              z: 85.46751512564336
+            },
+            orientation: {
+              heading: 271.628505216584,
+              pitch: -78.4,
+              roll: 359.3
+            },
+            hFOV: 32.5,
+            vFOV: 19,
+            hintLineVisible: true
           }
         }
       ]
     },
     {
-      //  视频投放图层
       id: '567-789-123-345',
       name: 'layer2',
       videoList: [
-        // 视频列表
         {
-          id: '543-321-987-765', // 视频id
-          name: 'layer2Video1', // 视频名称
-          description: '', // 描述
-          isProjected: false, // 是否开启视频投放
+          id: '765-987-543-321',
+          name: 'layer2Video1',
+          description: '',
+          isProjected: false,
           params: {
             videoSource: {
-              protocol: 'mp4', // 视频传输协议
-              videoUrl: 'http://localhost:8080/video/video1.mp4' // 视频服务地址
+              protocol: 'm3u8',
+              videoUrl:
+                'http://192.168.91.123:10008/record/video1/20211221/out.m3u8'
             },
-            cameraPosition: { x: 0, y: 0, z: 0 }, // 相机位置
+            cameraPosition: {
+              x: 114.401228136856,
+              y: 30.467421377675457,
+              z: 84.94989410478892
+            },
             orientation: {
-              heading: 0, // 方向角
-              pitch: 0, // 俯仰角
-              roll: 0 // 滚动角
+              heading: 6.053866507322313,
+              pitch: -73.6,
+              roll: 354.1
             },
-            hFOV: 15, // 水平视场角
-            vFOV: 15, // 垂直视场角
-            hintLineVisible: true // 是否显示投放区域线
+            hFOV: 34.6,
+            vFOV: 18.9,
+            hintLineVisible: true
+          }
+        },
+        {
+          id: '765-987-543-123',
+          name: 'layer2Video2',
+          description: '',
+          isProjected: false,
+          params: {
+            videoSource: {
+              protocol: 'm3u8',
+              videoUrl:
+                'http://192.168.91.123:10008/record/video2/20211221/out.m3u8'
+            },
+            cameraPosition: {
+              x: 114.40088870656619,
+              y: 30.467421563975016,
+              z: 84.91172691510191
+            },
+            orientation: {
+              heading: 359.89407747239846,
+              pitch: -74.2,
+              roll: 0
+            },
+            hFOV: 33.1,
+            vFOV: 19.2,
+            hintLineVisible: true
           }
         }
       ]
@@ -139,4 +209,9 @@ export default class MpVideo extends Mixins(WidgetMixin) {
   }
 }
 </script>
-<style lang="less"></style>
+<style lang="less">
+.video-manager {
+  width: 310px;
+  max-width: 100%;
+}
+</style>
