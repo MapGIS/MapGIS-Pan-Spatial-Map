d99f7364582d00212dce712d0af5ee245214a1c1
gongyuejian <gongyuejian.000@mapgis.com>
2022-01-05 13:45:33 +0800

    factor：优化视频投放单例

--- packages/pan-spatial-map-plugin-analysis/src/widgets/video/video.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/video/video.vue
@@ -2,17 +2,19 @@
   <mapgis-3d-video-manager
     class="video-manager"
     :videoOverlayLayerList="videoOverlayLayerList"
-    @load="load"
-    @update-videoOverlayLayerList="updateVideoOverlayLayerList"
     :modelUrl="modelUrl"
     :modelOffset="modelOffset"
+    :currentLayerId="currentLayerId"
+    :currentVideoId="currentVideoId"
+    @load="load"
+    @update-videoOverlayLayerList="updateVideoOverlayLayerList"
   >
   </mapgis-3d-video-manager>
 </template>
 <script lang="ts">
 import { Component, Mixins, Watch } from 'vue-property-decorator'
 import { WidgetMixin } from '@mapgis/web-app-framework'
-import { api, VideoOverlayLayerList } from '@mapgis/pan-spatial-map-common'
+import { api, VideoManager } from '@mapgis/pan-spatial-map-common'
 
 @Component({
   name: 'MpVideo'
@@ -52,132 +54,30 @@ export default class MpVideo extends Mixins(WidgetMixin) {
             vFOV: 18.9,
             hintLineVisible: true
           }
-        },
-        {
-          id: '987-765-543-123',
-          name: 'testVideo2',
-          description: '',
-          isProjected: false,
-          params: {
-            videoSource: {
-              protocol: 'm3u8',
-              videoUrl:
-                'http://192.168.91.123:10008/record/video2/20211221/out.m3u8'
-            },
-            cameraPosition: {
-              x: 114.40088870656619,
-              y: 30.467421563975016,
-              z: 84.91172691510191
-            },
-            orientation: {
-              heading: 359.89407747239846,
-              pitch: -74.2,
-              roll: 0
-            },
-            hFOV: 33.1,
-            vFOV: 19.2,
-            hintLineVisible: true
-          }
-        },
-        {
-          id: '987-765-543-124',
-          name: 'testVideo3',
-          description: '',
-          isProjected: false,
-          params: {
-            videoSource: {
-              protocol: 'm3u8',
-              videoUrl:
-                'http://192.168.91.123:10008/record/video3/20211221/out.m3u8'
-            },
-            cameraPosition: {
-              x: 114.4006886798949,
-              y: 30.467287432107295,
-              z: 85.46751512564336
-            },
-            orientation: {
-              heading: 271.628505216584,
-              pitch: -78.4,
-              roll: 359.3
-            },
-            hFOV: 32.5,
-            vFOV: 19,
-            hintLineVisible: true
-          }
-        }
-      ]
-    },
-    {
-      id: '567-789-123-345',
-      name: 'layer2',
-      videoList: [
-        {
-          id: '765-987-543-321',
-          name: 'layer2Video1',
-          description: '',
-          isProjected: false,
-          params: {
-            videoSource: {
-              protocol: 'm3u8',
-              videoUrl:
-                'http://192.168.91.123:10008/record/video1/20211221/out.m3u8'
-            },
-            cameraPosition: {
-              x: 114.401228136856,
-              y: 30.467421377675457,
-              z: 84.94989410478892
-            },
-            orientation: {
-              heading: 6.053866507322313,
-              pitch: -73.6,
-              roll: 354.1
-            },
-            hFOV: 34.6,
-            vFOV: 18.9,
-            hintLineVisible: true
-          }
-        },
-        {
-          id: '765-987-543-123',
-          name: 'layer2Video2',
-          description: '',
-          isProjected: false,
-          params: {
-            videoSource: {
-              protocol: 'm3u8',
-              videoUrl:
-                'http://192.168.91.123:10008/record/video2/20211221/out.m3u8'
-            },
-            cameraPosition: {
-              x: 114.40088870656619,
-              y: 30.467421563975016,
-              z: 84.91172691510191
-            },
-            orientation: {
-              heading: 359.89407747239846,
-              pitch: -74.2,
-              roll: 0
-            },
-            hFOV: 33.1,
-            vFOV: 19.2,
-            hintLineVisible: true
-          }
         }
       ]
     }
   ]
 
-  private VideoOverlayLayerListInstance = VideoOverlayLayerList
+  private VideoManagerInstance = VideoManager
 
   private get videoOverlayLayerList() {
-    const videoOverlayLayerList = this.VideoOverlayLayerListInstance.getVideoOverlayLayerList()
+    const videoOverlayLayerList = this.VideoManagerInstance.getVideoOverlayLayerList()
     return videoOverlayLayerList
   }
 
   private set videoOverlayLayerList(videoOverlayLayerList) {
-    this.VideoOverlayLayerListInstance.setVideoOverlayLayerList(
-      videoOverlayLayerList
-    )
+    this.VideoManagerInstance.setVideoOverlayLayerList(videoOverlayLayerList)
+  }
+
+  private get currentLayerId() {
+    const layerId = this.VideoManagerInstance.getCurrentLayerId()
+    return layerId
+  }
+
+  private get currentVideoId() {
+    const videoId = this.VideoManagerInstance.getCurrentVideoId()
+    return videoId
   }
 
   @Watch('videoOverlayLayerList', {
@@ -192,12 +92,15 @@ export default class MpVideo extends Mixins(WidgetMixin) {
 
   private videoComponent = null
 
-  load(videoComponent) {
-    this.videoComponent = videoComponent
+  mounted() {
     this.videoOverlayLayerList =
       (this.widgetInfo.config &&
         this.widgetInfo.config.videoOverlayLayerList) ||
       this.videoLayerList
+  }
+
+  load(videoComponent) {
+    this.videoComponent = videoComponent
   }
 
   onActive() {
