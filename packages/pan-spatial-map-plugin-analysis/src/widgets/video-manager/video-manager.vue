5ae3131b4849e1127eb238943d2fe8f60fc9d0c7
wangshun <wangshun@mapgis.com>
2022-01-17 11:25:19 +0800

    fix:视频投放点击设置按钮请求camera.glb文件错误

--- packages/pan-spatial-map-plugin-analysis/src/widgets/video-manager/video-manager.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/video-manager/video-manager.vue
@@ -21,7 +21,7 @@ import { api, VideoManager } from '@mapgis/pan-spatial-map-common'
   name: 'MpVideoManager'
 })
 export default class MpVideoManager extends Mixins(WidgetMixin) {
-  private modelUrl = './CesiumModels/Cesium_Camera.glb'
+  private modelUrl = `${process.env.BASE_URL}CesiumModels/Cesium_Camera.glb`
 
   private modelOffset = { headingOffset: -90, pitchOffset: 0, rollOffset: 0 }
 
