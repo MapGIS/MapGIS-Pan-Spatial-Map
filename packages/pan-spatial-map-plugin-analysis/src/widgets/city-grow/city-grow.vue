6a67d43ab01e82007ee217946f027bb11dfcc84a
zhangying-zy618 <1057936309@qq.com>
2022-01-27 16:42:39 +0800

    【粒子特效增加保存配置功能】【城市生长合并baseUrl输入方式、增加保存配置】

--- packages/pan-spatial-map-plugin-analysis/src/widgets/city-grow/city-grow.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/city-grow/city-grow.vue
@@ -1,128 +1,77 @@
 <template>
   <div class='mp-widget-city-grow'>
-<!--    <a-space direction='vertical' style='flex: 1'>-->
-      <a-row>
-        <label class='mp-widget-label'>MapGIS文档地址设置</label>
+    <a-row>
+      <label class='mp-widget-label'>MapGIS文档地址设置</label>
+    </a-row>
+    <div>
+      <a-row class='mp-row-style'>
+        <a-select
+          v-model='selectResult'
+          :show-search='true'
+          :not-found-content='null'
+          :filter-option='true'
+          :allowClear='true'
+          placeholder='请选择或输入文档地址'
+          @change='onUrlChange'
+          @search='handleSearch'
+          @blur='handleBlur'
+          style='width: 360px'
+        >
+          <a-select-option
+            v-for='item in urlOptions'
+            :key='item.baseUrl'
+          >
+            {{ item.baseUrl }}
+          </a-select-option>
+        </a-select>
+
+      </a-row>
+      <a-row style='height: 42px'>
+        <a-textarea
+          class='url-example'
+          disabled
+          :value='`示例 : ${this.urlExample}`'
+          auto-size
+        ></a-textarea>
       </a-row>
       <a-row>
-        <a-radio-group v-model='radioVal'>
-          <a-radio
-            :value='1'
-          >
-            选择现有文档地址
-          </a-radio>
-          <a-radio
-            :value='2'
-          >
-            输入新的文档地址
-          </a-radio>
-        </a-radio-group>
+        <label class='mp-widget-label'>参数设置</label>
       </a-row>
-      <div v-if='radioVal === 1'>
-        <a-row class='mp-row-style'>
-          <a-select
-            notFoundContent='请添加分类'
-            placeholder='请选择现有文档地址'
-            @change='onUrlChange'
-            style='width: 360px'
+      <mapgis-3d-city-grow-options
+        v-if='radioVal === 1'
+        :cityGrowOptions='cityGrowOptions'
+        style='width: 360px'
+        @commitOptions='getCityGrowOptions'
+        @saveConfig='saveConfig'
+        ref='cityGrowOptions'
+      ></mapgis-3d-city-grow-options>
+      <mp-window-wrapper :visible='startCityGrow'>
+        <template v-slot:default='slotProps'>
+          <mp-window
+            title='城市生长'
+            :visible.sync='startCityGrow'
+            :horizontal-offset='28'
+            :vertical-offset='30'
+            :width='playWidth'
+            :height='100'
+            :has-padding='false'
+            anchor='bottom-center'
+            v-bind='slotProps'
           >
-            <a-select-option
-              v-for='item in urlOptions'
-              :key='item.baseUrl'
-            >
-              {{ item.baseUrl }}
-            </a-select-option>
-          </a-select>
-        </a-row>
-        <a-row>
-          <label class='mp-widget-label'>参数设置</label>
-        </a-row>
-        <mapgis-3d-city-grow-options
-          v-if='radioVal === 1'
-          :cityGrowOptions='cityGrowOptions'
-          style='width: 360px'
-          @commitOptions='getCityGrowOptions'
-        ></mapgis-3d-city-grow-options>
-        <mp-window-wrapper :visible='startCityGrow'>
-          <template v-slot:default='slotProps'>
-            <mp-window
-              title='城市生长'
-              :visible.sync='startCityGrow'
-              :horizontal-offset='28'
-              :vertical-offset='30'
-              :width='playWidth'
-              :height='100'
-              :has-padding='false'
-              anchor='bottom-center'
-              v-bind='slotProps'
-            >
-              <template>
-                <mapgis-3d-city-grow
-                  v-if='startCityGrow'
-                  :baseUrl='cityGrowOptions.baseUrl'
-                  :featureStyle='featureStyle'
-                  :width='playWidth - 20'
-                  ref='cityGrow'
-                  @loaded='load'
-                ></mapgis-3d-city-grow>
-              </template>
-            </mp-window>
-          </template>
-        </mp-window-wrapper>
-      </div>
-      <div v-else>
-        <a-row class='mp-row-style'>
-          <a-input-search
-            placeholder='基地址'
-            enter-button='确认'
-            @search='searchFields'>
-          </a-input-search>
-        </a-row>
-        <a-row>
-          <a-textarea
-            class='url-example'
-            disabled
-            :value='`示例 : ${this.urlExample}`'
-            auto-size
-          ></a-textarea>
-        </a-row>
-        <a-row>
-          <label class='mp-widget-label'>参数设置</label>
-        </a-row>
-        <mapgis-3d-city-grow-options
-          v-if='radioVal === 2'
-          :cityGrowOptions='url'
-          @commitOptions='getCityGrowOptions'
-          style='width: 360px'
-        ></mapgis-3d-city-grow-options>
-        <mp-window-wrapper :visible='startCityGrow'>
-          <template v-slot:default='slotProps'>
-            <mp-window
-              title='城市生长'
-              :visible.sync='startCityGrow'
-              :horizontal-offset='28'
-              :vertical-offset='30'
-              :width='playWidth'
-              :height='100'
-              :has-padding='false'
-              anchor='bottom-center'
-              v-bind='slotProps'
-            >
-              <template>
-                <mapgis-3d-city-grow
-                  v-if='startCityGrow'
-                  :baseUrl='url'
-                  :featureStyle='featureStyle'
-                  :width='playWidth - 20'
-                  ref='cityGrow'
-                  @loaded='load'
-                ></mapgis-3d-city-grow>
-              </template>
-            </mp-window>
-          </template>
-        </mp-window-wrapper>
-      </div>
-<!--    </a-space>-->
+            <template>
+              <mapgis-3d-city-grow
+                v-if='startCityGrow'
+                :baseUrl='url'
+                :featureStyle='featureStyle'
+                :width='playWidth - 20'
+                ref='cityGrow'
+                @loaded='load'
+              ></mapgis-3d-city-grow>
+            </template>
+          </mp-window>
+        </template>
+      </mp-window-wrapper>
+    </div>
   </div>
 </template>
 
@@ -132,6 +81,7 @@ import {
   WidgetMixin,
   UrlUtil
 } from '@mapgis/web-app-framework'
+import { api } from '@mapgis/pan-spatial-map-common'
 
 @Component({
   name: 'MpCityGrow'
@@ -140,61 +90,123 @@ export default class MpCityGrow extends Mixins(WidgetMixin) {
 
   private radioVal = 1
 
+  private selectResult = ''
+
   private url = ''
 
-  private dataList = []
+  private cityGrowOptions = { }
 
-  private cityGrowOptions = {}
-
-  private urlExample = 'http://192.168.21.192:6163/igs/rest/mrfs/docs/shengZhenBaiMo/0/0'
+  private urlExample = 'http://192.168.21.191:6163/igs/rest/mrfs/docs/shengZhenBaiMo/0/0'
 
   private featureStyle = {}
 
   private startCityGrow = false
 
-  private playWidth = 600
+  private playWidth = 720
 
   // 城市生长对象
   private cityGrow = null
 
-  private get urlOptions() {
-    const config = this.widgetInfo.config
-    this.dataList = config
-    return config
+  private handleSearchTag = false
+
+  private urlOptionsArray =
+    [
+      {
+        'baseUrl': 'http://192.168.21.191:6163/igs/rest/mrfs/docs/shengZhenBaiMo/0/0',
+        'startTimeField': 'startTime',
+        'endTimeField': 'endTime',
+        'heightField': 'height'
+      }
+    ]
+
+  private urlOptions = []
+
+
+  async mounted() {
+    const config = await api.getWidgetConfig('city-grow')
+    this.urlOptions = config || this.urlOptionsArray
   }
 
-  load(e){
-    this.cityGrow = e;
+  load(e) {
+    this.cityGrow = e
   }
 
   onUrlChange(val) {
-    const mapOption = this.dataList.filter(x =>
-      x.baseUrl === val)
+    if (val && !this.handleSearchTag) {
+      const mapOption = this.urlOptions.filter(x =>
+        x.baseUrl === val)
 
-    this.cityGrowOptions = mapOption[0]
+      this.cityGrowOptions = mapOption[0]
+      this.url = val
+    } else if (val === undefined) {
+      this.cityGrowOptions = {}
+    }
   }
 
-  searchFields(value) {
-    if (!UrlUtil.isUrlValid(value)) {
-      this.$message.warn('请输入正确的数据地址')
-      return
+  handleBlur(value) {
+    if (value == undefined){
+      this.selectResult = this.cityGrowOptions
     }
-    this.url = value
+  }
+
+  handleSearch(value) {
+    if (value !== '') {
+      if (!UrlUtil.isUrlValid(value)) {
+        this.$message.warn('请输入正确的数据地址')
+        return
+      }
+      this.cityGrowOptions = value
+      this.url = value
+      this.handleSearchTag = true
+    } else {
+      this.handleSearchTag = false
+    }
   }
 
   getCityGrowOptions(featureStyle) {
     this.featureStyle = {}
     this.$nextTick(function() {
       this.featureStyle = featureStyle
+      this.saveConfig()
     })
     this.startCityGrow = true
   }
 
+  saveConfig() {
+    let findTag = false
+    const config = JSON.parse(JSON.stringify(this.urlOptions))
+    this.featureStyle.baseUrl = this.url
+
+    for (let i = 0; i < this.urlOptions.length; i++) {
+      if (this.urlOptions[i].baseUrl === this.url){
+        config[i] = this.featureStyle
+        findTag = true
+      }
+    }
+    if (!findTag){
+      config.push(this.featureStyle)
+    }
+
+    api
+      .saveWidgetConfig({
+        name: 'city-grow',
+        config: config
+      })
+      .then(() => {
+        console.log('更新城市生长配置成功')
+      })
+      .catch(() => {
+        console.log('更新城市生长配置失败')
+      })
+  }
+
   onClose() {
     this.hideCityGrow()
-    if (this.cityGrow){
+    if (this.cityGrow) {
       this.cityGrow.unmount()
     }
+    this.$refs.cityGrowOptions.remove()
+    this.selectResult = ''
   }
 
   hideCityGrow() {
@@ -205,15 +217,16 @@ export default class MpCityGrow extends Mixins(WidgetMixin) {
 </script>
 
 <style lang='less' scoped>
-.mp-widget-city-grow{
+.mp-widget-city-grow {
   margin: 10px;
 }
+
 .mp-row-style {
   width: 360px;
   margin: 8px 0;
 }
 
-.mp-widget-label{
+.mp-widget-label {
   width: 41px;
   height: 12px;
   font-size: 14px;
--- packages/pan-spatial-map-plugin-analysis/src/widgets/particle-effects/particle-effects.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/particle-effects/particle-effects.vue
@@ -4,7 +4,8 @@
       id='mp-3d-particle-effects'
       :symbolList='symbolList'
       :particleList='particleList'
-      @changeParticel='changeParticle'
+      ref='particleEffect'
+      @changeParticle = 'changeParticle'
       @load='load'
     />
   </div>
@@ -13,6 +14,7 @@
 <script lang='ts'>
 import { Mixins, Component } from 'vue-property-decorator'
 import { WidgetMixin, Objects } from '@mapgis/web-app-framework'
+import { api } from '@mapgis/pan-spatial-map-common'
 
 declare function require(string): string;
 
@@ -21,17 +23,26 @@ export default class MpParticleEffects extends Mixins(WidgetMixin) {
 
   private particleEffects = null
 
+  private particleListArray = []
+
   private particleList = []
 
-  get symbolList() {
-    return this.widgetInfo.config.symbolList.map(item => {
-      return {
-        guid: item.guid,
-        name: item.name,
-        image: `${this.baseUrl}${item.image}`,
-        iconUrl: item.iconUrl
-      }
-    })
+  private symbolList = []
+
+  private particleChangedList = []
+
+  async mounted(){
+    const config = await api.getWidgetConfig('particle-effects')
+    this.symbolList = config.symbolList.map(item => {
+          return {
+            guid: item.guid,
+            name: item.name,
+            image: `${this.baseUrl}${item.image}`,
+            iconUrl: item.iconUrl,
+            config: item.config
+          }
+        })
+    this.particleListArray = config.particleListConfig
   }
 
   load(particleEffects) {
@@ -40,17 +51,17 @@ export default class MpParticleEffects extends Mixins(WidgetMixin) {
 
   onOpen() {
     this.particleEffects.mount()
-    this.particleList = this.widgetInfo.config.particleListConfig
+    this.particleList = this.particleListArray
   }
 
   onClose() {
     this.particleEffects.unmount()
-    // this.particleList = []
   }
 
-  changeParticle(e) {
-    console.log('particleList', e)
+  changeParticle(particleList) {
+    this.particleChangedList = particleList
   }
+
 
   // 微件窗口模式切换时回调
   onWindowSize(mode) {
@@ -62,6 +73,60 @@ export default class MpParticleEffects extends Mixins(WidgetMixin) {
       }
     })
   }
+
+  // 微件失活时
+  onDeActive() {
+    // 微件失活时自动保存配置到后台
+    this.saveConfig()
+    // this.particleEffects.unmount()
+  }
+
+  async saveConfig() {
+    const originConfig = await api.getWidgetConfig('particle-effects')
+    originConfig.particleListConfig = this.recursion(this.particleChangedList);
+    api
+      .saveWidgetConfig({
+        name: 'particle-effects',
+        config: JSON.stringify(originConfig)
+      })
+      .then(() => {
+        console.log('更新particle配置成功')
+      })
+      .catch(() => {
+        console.log('更新particle配置失败')
+      })
+  }
+
+  // 递归删除对象数组中的__ob__属性
+  recursion(obj) {
+    const vm = this
+    if(obj instanceof Array){
+      const newOnj = [];
+      for (let i =0;i<obj.length;i++){
+        if(obj[i] instanceof Object){
+          newOnj.push(vm.recursion(obj[i]));
+        }else {
+          newOnj.push(obj[i]);
+        }
+      }
+      return newOnj;
+    }else if(obj instanceof Object){
+      const newOnj = {};
+      Object.keys(obj).forEach(function(key) {
+        if (obj[key] instanceof Object){
+          if(key !== '__ob__'){
+            newOnj[key] = vm.recursion(obj[key]);
+          }
+        }else {
+          newOnj[key] = obj[key];
+        }
+      })
+      return newOnj;
+    }else {
+      return obj;
+    }
+  }
+
 
 }
 </script>
