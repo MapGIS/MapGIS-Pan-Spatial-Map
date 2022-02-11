57624d4e531c524d8587274136d3e39c22f6293a
gongyuejian <gongyuejian.000@mapgis.com>
2022-02-11 10:26:53 +0800

    fix：可视域分析面板关闭后再打开无法进行分析问题修复

--- packages/pan-spatial-map-plugin-analysis/src/widgets/visual-analysis/visual-analysis.vue
+++ packages/pan-spatial-map-plugin-analysis/src/widgets/visual-analysis/visual-analysis.vue
@@ -1,25 +1,23 @@
 <template>
   <mapgis-3d-viewshed
-    :horizontAngle='horizontAngle'
-    :verticalAngle='verticalAngle'
-    :unVisibleColor='unVisibleColor'
-    :visibleColor='visibleColor'
-    :maskColor='maskColor'
-    :exHeight='exHeight'
-    @load='load'></mapgis-3d-viewshed>
+    :horizontAngle="horizontAngle"
+    :verticalAngle="verticalAngle"
+    :unVisibleColor="unVisibleColor"
+    :visibleColor="visibleColor"
+    :maskColor="maskColor"
+    :exHeight="exHeight"
+    @load="load"
+  ></mapgis-3d-viewshed>
 </template>
 
 <script lang="ts">
 import { Mixins, Component } from 'vue-property-decorator'
-import {
-  WidgetMixin
-} from '@mapgis/web-app-framework'
+import { WidgetMixin } from '@mapgis/web-app-framework'
 
 @Component({
   name: 'MpVisualAnalysis'
 })
 export default class MpVisualAnalysis extends Mixins(WidgetMixin) {
-
   // 观察点的水平视角
   private horizontAngle = 60
 
@@ -41,6 +39,12 @@ export default class MpVisualAnalysis extends Mixins(WidgetMixin) {
   // 可视域分析对象
   private visualAnalysis = null
 
+  onOpen() {
+    if (this.visualAnalysis) {
+      this.visualAnalysis.mount()
+    }
+  }
+
   onClose() {
     this.visualAnalysis.unmount()
   }
@@ -51,5 +55,4 @@ export default class MpVisualAnalysis extends Mixins(WidgetMixin) {
 }
 </script>
 
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
