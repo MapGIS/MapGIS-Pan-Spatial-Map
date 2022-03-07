6e946b1847ace4c0bb9d7bffc316e17a05677787
gongyuejian <gongyuejian.000@mapgis.com>
2022-03-07 09:23:15 +0800

    fix：二维分段专题服务弹框使用marker的弹框，弹框显示内容同步配置里的信息

--- packages/pan-spatial-map-plugin-editor/src/widgets/thematic-map/components/ThematicMapLayers/components/Mapbox/SubSectionMap.vue
+++ packages/pan-spatial-map-plugin-editor/src/widgets/thematic-map/components/ThematicMapLayers/components/Mapbox/SubSectionMap.vue
@@ -1,25 +1,48 @@
 <template>
-  <!-- 分段专题图 -->
-  <mapgis-theme-layer-custom
-    @highlightChanged="emitHighlight"
-    :theme-options="themeOptions"
-    :show-panel="false"
-    :enable-tips="true"
-    :tips-options="tipsOptions"
-    :layer-id="id"
-    :field="field"
-    :data-source="geojson"
-    :highlight-feature="marker.feature"
-    type="range"
-    ref="customRangeThemeLayer"
-  />
+  <div>
+    <!-- 分段专题图 -->
+    <mapgis-theme-layer-custom
+      @highlightChanged="emitHighlight"
+      :theme-options="themeOptions"
+      :show-panel="false"
+      :enable-tips="false"
+      :tips-options="tipsOptions"
+      :layer-id="id"
+      :field="field"
+      :data-source="geojson"
+      :highlight-feature="marker.feature"
+      type="range"
+      ref="customRangeThemeLayer"
+      @rangeLayer="getRangeLayer"
+    />
+    <mp-marker-pro
+      :marker="selfMarker"
+      v-if="selfMarker.fid"
+      :defaultShowPopup="true"
+    />
+  </div>
 </template>
 <script lang="ts">
-import { Mixins, Component } from 'vue-property-decorator'
+import { Mixins, Component, Inject, Watch } from 'vue-property-decorator'
 import BaseMixin from '../../mixins/base'
+import { getMarker, IMarker } from '../../../../utils'
+import _debounce from 'lodash/debounce'
 
 @Component
 export default class MapboxSubSectionMap extends Mixins(BaseMixin) {
+  @Inject('map') map
+
+  /**
+   * 监听：图属联动变化
+   */
+  @Watch('marker.fid')
+  fidChanged() {
+    this.selfMarker = this.marker
+  }
+
+  // 标注
+  private selfMarker: IMarker | Record<string, unknown> = {}
+
   private tipsOptions = {
     enableHighlight: true,
     type: 'point'
@@ -45,11 +68,30 @@ export default class MapboxSubSectionMap extends Mixins(BaseMixin) {
     }
   }
 
+  getRangeLayer(layer) {
+    console.log(layer)
+    const { map } = this
+    map.on(
+      'mousemove',
+      _debounce(e => {
+        const f = map.queryRenderedFeatures(e.point, {
+          layers: [layer.id]
+        })
+        if (!f || f.length < 1) return
+        const fid = f[0].id
+        getMarker(this.geojson, fid, this.propertiesOption).then(
+          marker => (this.selfMarker = marker || {})
+        )
+      }, 200)
+    )
+  }
+
   removeLayer() {
     const rangeLayer = this.$refs.customRangeThemeLayer
     if (rangeLayer) {
       rangeLayer.resetLayer(this.id)
     }
+    this.map.off('mousemove')
   }
 }
 </script>
