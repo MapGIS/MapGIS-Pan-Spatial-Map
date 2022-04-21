7f01e6d6251ec460b3a33ecb03a0135477b70f08
左宛莹 <767030886@qq.com>
2022-04-21 11:13:56 +0800

    设置framework兼容ico格式的图片，以便用来设计鼠标样式

--- packages/pan-spatial-map-framework/vue.config.js
+++ packages/pan-spatial-map-framework/vue.config.js
@@ -10,12 +10,12 @@ const productionGzipExtensions = ['js', 'css']
 const isProd = process.env.NODE_ENV === 'production'
 const name = defaultSettings.title
 
-console.log('NODE_ENV=' + process.env.NODE_ENV)
+console.log(`NODE_ENV=${process.env.NODE_ENV}`)
 
 module.exports = {
   devServer: {
     hot: true,
-    open: true
+    open: true,
     // proxy: {
     //   '/api': { //此处要与 /services/api.js 中的 API_PROXY_PREFIX 值保持一致
     //     target: process.env.VUE_APP_API_BASE_URL,
@@ -29,21 +29,21 @@ module.exports = {
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
-      patterns: [path.resolve(__dirname, './src/theme/theme.less')]
-    }
+      patterns: [path.resolve(__dirname, './src/theme/theme.less')],
+    },
   },
-  configureWebpack: config => {
+  configureWebpack: (config) => {
     config.name = name
     config.entry.app = ['babel-polyfill', 'whatwg-fetch', './src/main.js']
     config.performance = {
-      hints: false
+      hints: false,
     }
     config.plugins.push(
       new ThemeColorReplacer({
         fileName: 'css/theme-colors-[contenthash:8].css',
         matchColors: getThemeColors(),
         injectCss: true,
-        resolveCss
+        resolveCss,
       })
     )
     // Ignore all locale files of moment.js
@@ -56,20 +56,28 @@ module.exports = {
           algorithm: 'gzip',
           test: new RegExp(`\\.(${productionGzipExtensions.join('|')})$`),
           threshold: 10240,
-          minRatio: 0.8
+          minRatio: 0.8,
         })
       )
     }
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     // 生产环境下关闭css压缩的 colormin 项，因为此项优化与主题色替换功能冲突
     if (isProd) {
-      config.plugin('optimize-css').tap(args => {
+      config.plugin('optimize-css').tap((args) => {
         args[0].cssnanoOptions.preset[1].colormin = false
         return args
       })
     }
-    config.plugin('fork-ts-checker').tap(args => {
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|ico)$/i)
+      .use('url-loader')
+      .loader('url-loader')
+      .tap((options) =>
+        Object.assign(options, { limit: 2000, esModule: false })
+      )
+    config.plugin('fork-ts-checker').tap((args) => {
       args[0].memoryLimit = 12288
       return args
     })
@@ -79,13 +87,13 @@ module.exports = {
       less: {
         lessOptions: {
           modifyVars: modifyVars(),
-          javascriptEnabled: true
-        }
-      }
-    }
+          javascriptEnabled: true,
+        },
+      },
+    },
   },
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
   outputDir: 'dist',
   assetsDir: 'static',
-  productionSourceMap: false
+  productionSourceMap: false,
 }
