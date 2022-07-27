6ea573b25d83d048dd60ee598f77b65ab02878f3
gongyuejian <gongyuejian.000@mapgis.com>
2022-07-27 17:05:09 +0800

    feat：三维模型支持模型阴影区域亮度设置

--- packages/pan-spatial-map-plugin-workspace/src/components/TreeLayer/components/ChangeM3DProps/ChangeM3DProps.vue
+++ packages/pan-spatial-map-plugin-workspace/src/components/TreeLayer/components/ChangeM3DProps/ChangeM3DProps.vue
@@ -9,7 +9,7 @@
         <a-input-number
           v-model="maximumScreenSpaceError"
           :min="0"
-          style="width:100%"
+          style="width: 100%"
         />
       </a-form-item>
       <a-form-item label="开启拾取">
@@ -18,10 +18,16 @@
       <a-form-item label="开启多模态切换">
         <a-switch v-model="enableModelSwitch" />
       </a-form-item>
-      <div style="textAlign:right">
-        <a-button type="primary" @click="submit">
-          确认
-        </a-button>
+      <mapgis-ui-input-number-panel
+        size="large"
+        label="模型阴影区域亮度调节"
+        v-model="luminanceAtZenith"
+        :range="[1, 200]"
+        :step="0.2"
+        @change="luminanceAtZenithChange"
+      />
+      <div style="textalign: right">
+        <a-button type="primary" @click="submit"> 确认 </a-button>
       </div>
     </a-form>
   </div>
@@ -40,13 +46,24 @@ export default class MpChangeM3DProps extends Vue {
 
   maximumScreenSpaceError = this.layer.maximumScreenSpaceError
 
-  enableModelSwitch = this.layer.layer
-    ? this.layer.layer.modelSwitchEnabled
-    : this.layer.modelSwitchEnabled
+  private enableModelSwitch = false
 
-  enablePopup = this.layer.layer
-    ? this.layer.layer.popupEnabled
-    : this.layer.popupEnabled
+  private enablePopup = false
+
+  private luminanceAtZenith = 10
+
+  @Watch('layer', { immediate: true, deep: true })
+  initParams() {
+    const layer = this.layer.layer ? this.layer.layer : this.layer
+
+    this.enableModelSwitch =
+      layer && layer.modelSwitchEnabled ? layer.modelSwitchEnabled : false
+
+    this.enablePopup = layer && layer.popupEnabled ? layer.popupEnabled : false
+
+    this.luminanceAtZenith =
+      layer && layer.luminanceAtZenith ? layer.luminanceAtZenith : 10
+  }
 
   submit() {
     if (this.layer.maximumScreenSpaceError) {
@@ -55,11 +72,22 @@ export default class MpChangeM3DProps extends Vue {
     if (this.layer.layer) {
       this.layer.layer.popupEnabled = this.enablePopup
       this.layer.layer.modelSwitchEnabled = this.enableModelSwitch
+      this.layer.layer.luminanceAtZenith = this.luminanceAtZenith
     } else {
       this.layer.popupEnabled = this.enablePopup
       this.layer.modelSwitchEnabled = this.enableModelSwitch
+      this.layer.luminanceAtZenith = this.luminanceAtZenith
     }
     this.$emit('update:layer', this.layer)
+  }
+
+  luminanceAtZenithChange() {
+    if (this.layer.layer) {
+      this.layer.layer.luminanceAtZenith = this.luminanceAtZenith
+    } else {
+      this.layer.luminanceAtZenith = this.luminanceAtZenith
+    }
+    this.$emit('update:luminanceAtZenith', this.layer)
   }
 }
 </script>
