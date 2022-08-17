d0f93984209262c9191bef3ccbfdf298a1778816
王魁帅 <2692537835@qq.com>
2022-08-17 11:31:07 +0800

    feat:左侧微件支持在manifest中自定义宽度

--- packages/pan-spatial-map-plugin-theme/src/components/SidePanel/SideCard.vue
+++ packages/pan-spatial-map-plugin-theme/src/components/SidePanel/SideCard.vue
@@ -4,6 +4,7 @@
     :title="widgetInfo.label"
     :icon="widgetInfo.icon"
     :is-full-screen="widgetInfo.properties.windowSize === 'max'"
+    :width="widgetInfo.properties.customWidth"
     :has-padding="widgetInfo.properties.hasPadding"
     :max-width="maxWidth"
     :visible="visible"
