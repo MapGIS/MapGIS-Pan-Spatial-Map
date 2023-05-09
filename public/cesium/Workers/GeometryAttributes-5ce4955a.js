70e2d6784233b112ed7299ee17bff07006ca5a46
xiongbo <xiongbo@mapgis.com>
2023-05-09 13:56:14 +0800

    style: add cesium resource

---
+++ public/cesium/Workers/GeometryAttributes-5ce4955a.js
@@ -0,0 +1,1 @@
+define(["exports","./when-ae2e0b60"],function(t,e){"use strict";t.GeometryAttributes=function(t){t=e.defaultValue(t,e.defaultValue.EMPTY_OBJECT),this.position=t.position,this.normal=t.normal,this.st=t.st,this.bitangent=t.bitangent,this.tangent=t.tangent,this.color=t.color}});
