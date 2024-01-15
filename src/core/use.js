58717bee80704b2326642342ac2c3661ebc3d20c
gongyuejian <gongyuejian.000@mapgis.com>
2024-01-15 15:31:41 +0800

    update use

--- src/core/use.js
+++ src/core/use.js
@@ -12,7 +12,6 @@ import MapgisUi from '@mapgis/webclient-vue-ui'
 import WebAppFrameworkUI from '@mapgis/web-app-framework'
 import Theme from '@mapgis/mapgis-pan-spatial-map-theme'
 import WebAppWidget from '@mapgis/mapgis-pan-spatial-map-widgets'
-import WebAppWidgetPro from '@mapgis/mapgis-pan-spatial-map-widgets-pro'
 import Antd from 'ant-design-vue/es'
 import HeaderAvatar from '@/components/HeaderAvatar'
 import About from '@/components/About'
@@ -27,6 +26,5 @@ Vue.use(Theme, {
   }
 })
 Vue.use(WebAppWidget)
-Vue.use(WebAppWidgetPro)
 
 process.env.NODE_ENV !== 'production' && console.warn('[antd-pro] WARNING: Antd now use fulled imported.')
