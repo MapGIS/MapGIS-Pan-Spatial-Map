ae34c05ed127dc8cd344351180953be54a753cbb
zhouzhenxiong <zhouzhenxiong.zzx@mapgis.com>
2024-10-25 16:47:12 +0800

    feat(3079):一张图支持可视化搭建

--- src/core/use.js
+++ src/core/use.js
@@ -1,18 +1,17 @@
 import Vue from 'vue'
 
 // base library
-// import '@mapgis/webclient-vue-mapboxgl/dist-libs/webclient-vue-mapboxgl.css'
-// import '@mapgis/webclient-vue-cesium/dist-libs/webclient-vue-cesium.css'
-// import '@mapgis/webclient-vue-ui/dist-libs/webclient-vue-ui.css'
-// import '@mapgis/web-app-framework/dist-libs/web-app-framework.css'
-// import '@mapgis/mapgis-pan-spatial-map-widgets/dist-libs/mapgis-pan-spatial-map-widgets.css'
-// import '@mapgis/mapgis-pan-spatial-map-theme/dist-libs/mapgis-pan-spatial-map-theme.css'
+import '@mapgis/webclient-vue-mapboxgl/dist-libs/webclient-vue-mapboxgl.css'
+import '@mapgis/webclient-vue-cesium/dist-libs/webclient-vue-cesium.css'
+import '@mapgis/webclient-vue-ui/dist-libs/webclient-vue-ui.css'
+import '@mapgis/web-app-framework/dist-libs/web-app-framework.css'
+import '@mapgis/mapgis-pan-spatial-map-widgets/dist-libs/mapgis-pan-spatial-map-widgets.css'
+import '@mapgis/mapgis-pan-spatial-map-theme/dist-libs/mapgis-pan-spatial-map-theme.css'
 
 import MapgisUi from '@mapgis/webclient-vue-ui'
 import WebAppFrameworkUI from '@mapgis/web-app-framework'
 import Theme from '@mapgis/mapgis-pan-spatial-map-theme'
 import WebAppWidget from '@mapgis/mapgis-pan-spatial-map-widgets'
-// import WebAppWidgetPro from '@mapgis/mapgis-pan-spatial-map-widgets-pro'
 import WidgetConfigUi from '@mapgis/mapgis-pan-spatial-map-widgets-config-ui'
 import Antd from 'ant-design-vue/es'
 import HeaderAvatar from '@/components/HeaderAvatar'
@@ -24,7 +23,7 @@ import draggable from 'vuedraggable'
 Vue.use(MapgisUi)
 Vue.use(Antd)
 Vue.use(WebAppFrameworkUI)
-// Vue.use(WebAppWidgetPro)
+// Vue.use(widgetsFrame)
 Vue.use(Theme, {
   components: {
     MpPanSpatialMapHeaderAvatar: HeaderAvatar,
@@ -33,10 +32,8 @@ Vue.use(Theme, {
 })
 Vue.use(WebAppWidget)
 Vue.use(WidgetConfigUi)
-// Vue.use(widgetsFrame)
 // Vue.component('MpPanSpatialMapHeaderAvatar', HeaderAvatar)
 // Vue.component('MpPanSpatialMapAbout', About)
-
 Vue.component('draggable', draggable)
 
 window['@mapgis/web-app-framework'] = require('@mapgis/web-app-framework')
