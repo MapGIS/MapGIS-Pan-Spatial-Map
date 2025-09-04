efe1d323446969e5578e1ba7142227e5c0f1945e
zhouzhenxiong <zhouzhenxiong.zzx@mapgis.com>
2025-09-04 10:57:50 +0000

    fix:修订应用搭建保存后预览时地图视图位同步问题
    (cherry picked from commit 45a34de2896e9a5ba110e3361b840e9e96f26599)
    
    (cherry picked from commit ad39e70e153ee4a7e6d5c299f2e3aee7e81df37b)

--- src/views/app/map/index.vue
+++ src/views/app/map/index.vue
@@ -35,6 +35,7 @@ export default {
       // 更新云门户服务的token信息，保证服务能够正常访问
       this.updatePortalDataCatologTokenInfo()
       this.application.document = AppManager.getInstance().generateDocument(this.application.document.maprender)
+      baseConfigInstance.config = this.application.baseConfig
     } else {
       this.application = AppManager.getInstance().getApplication()
       /**
