0f85fc58b6b27ec91520fdace0a4493ecc588894
gongyuejian <gongyuejian.000@mapgis.com>
2025-10-09 16:38:41 +0800

    fix: 拷贝命令增加拷贝common库的资源 问题原因: 拷贝命令增加拷贝common库的资源 修改说明: 拷贝命令增加拷贝common库的资源 接口变动: 否 同步版本: 10.7.6.10、10.7.6.10-pro、dev、dev-pro、feat/3078 统查统改: 是，发现同类问题0处

--- shell/copy-assets.js
+++ shell/copy-assets.js
@@ -1,9 +1,13 @@
 const fs = require('fs-extra')
 const path = require('path')
-const source = path.join(
+const sourceCesiumPlugin = path.join(
   __dirname,
   '../node_modules/@mapgis/webclient-cesium-plugin/dist/webclient-cesium-plugin-resource'
 )
-const dest = path.join(__dirname, '../public/webclient-cesium-plugin-resource')
-fs.copySync(source, dest)
+const destCesiumPlugin = path.join(__dirname, '../public/webclient-cesium-plugin-resource')
+fs.copySync(sourceCesiumPlugin, destCesiumPlugin)
+
+const sourceCommon = path.join(__dirname, '../node_modules/@mapgis/webclient-common/dist/webclient-common-resource')
+const destCommon = path.join(__dirname, '../public/webclient-common-resource')
+fs.copySync(sourceCommon, destCommon)
 console.log('✅ 复制资源完成')
